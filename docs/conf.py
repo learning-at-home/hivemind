ef7c9e183baadcc2c1f9f09131c5aea7a4ed7181
justheuristic <justheuristic@gmail.com>
2020-03-19 03:39:51 +0300

    config

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ extensions = [
 
 
 # Napoleon settings
-napoleon_google_docstring = True
+napoleon_google_docstring = False
 napoleon_numpy_docstring = True
 napoleon_include_init_with_doc = True
 napoleon_include_private_with_doc = False
