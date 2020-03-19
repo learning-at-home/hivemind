b122b253c100c9a9821aafbe305ad2a7e6d97b66
Restyled.io <commits@restyled.io>
2020-03-19 17:35:38 +0000

    Restyled by black

--- tesseract/runtime/__init__.py
+++ tesseract/runtime/__init__.py
@@ -35,19 +35,29 @@ class TesseractRuntime(threading.Thread):
       If you want to manually specify devices for each expert (in their forward pass), leave device=None (default)
     """
 
-    def __init__(self, expert_backends: Dict[str, ExpertBackend], prefetch_batches=64, sender_threads: int = 1,
-                 device: torch.device = None):
+    def __init__(
+        self,
+        expert_backends: Dict[str, ExpertBackend],
+        prefetch_batches=64,
+        sender_threads: int = 1,
+        device: torch.device = None,
+    ):
         super().__init__()
         self.expert_backends = expert_backends
-        self.pools = tuple(chain(*(expert.get_pools()
-                                   for expert in expert_backends.values())))
-        self.device, self.prefetch_batches, self.sender_threads = device, prefetch_batches, sender_threads
+        self.pools = tuple(
+            chain(*(expert.get_pools() for expert in expert_backends.values()))
+        )
+        self.device, self.prefetch_batches, self.sender_threads = (
+            device,
+            prefetch_batches,
+            sender_threads,
+        )
         self.shutdown_recv, self.shutdown_send = mp.Pipe(duplex=False)
         # event is set iff server is currently running and ready to accept batches
         self.ready = mp.Event()
 
     def run(self):
-        progress = tqdm.tqdm(bar_format='{desc}, {rate_fmt}')
+        progress = tqdm.tqdm(bar_format="{desc}, {rate_fmt}")
         for pool in self.pools:
             if not pool.is_alive():
                 pool.start()
@@ -59,12 +69,14 @@ class TesseractRuntime(threading.Thread):
             try:
                 self.ready.set()
                 for pool, batch_index, batch in BackgroundGenerator(
-                        self.iterate_minibatches_from_pools(), self.prefetch_batches):
+                    self.iterate_minibatches_from_pools(), self.prefetch_batches
+                ):
                     outputs = pool.process_func(*batch)
                     output_sender_pool.apply_async(
-                        pool.send_outputs_from_runtime, args=[batch_index, outputs])
+                        pool.send_outputs_from_runtime, args=[batch_index, outputs]
+                    )
                     progress.update(len(outputs[0]))
-                    progress.desc = f'pool.uid={pool.uid} batch_size={len(outputs[0])}'
+                    progress.desc = f"pool.uid={pool.uid} batch_size={len(outputs[0])}"
             finally:
                 self.shutdown()
 
@@ -84,8 +96,7 @@ class TesseractRuntime(threading.Thread):
         Chooses pool according to priority, then copies exposed batch and frees the buffer
         """
         with DefaultSelector() as selector:
-            selector.register(self.shutdown_recv, EVENT_READ,
-                              self.SHUTDOWN_TRIGGER)
+            selector.register(self.shutdown_recv, EVENT_READ, self.SHUTDOWN_TRIGGER)
             for pool in self.pools:
                 selector.register(pool.batch_receiver, EVENT_READ, pool)
 
@@ -99,5 +110,6 @@ class TesseractRuntime(threading.Thread):
                 pool = max(ready_objects, key=lambda pool: pool.priority)
 
                 batch_index, batch_tensors = pool.load_batch_to_runtime(
-                    timeout, self.device)
+                    timeout, self.device
+                )
                 yield pool, batch_index, batch_tensors
