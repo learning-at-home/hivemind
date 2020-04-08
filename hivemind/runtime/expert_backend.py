1e4cef0d85007b5dc0a65c62fe61d1cec5014fc6
Max Ryabinin <mryabinin0@gmail.com>
2020-04-08 11:19:10 +0300

    Reflect current state of affairs with randomness in docstrings

--- hivemind/runtime/expert_backend.py
+++ hivemind/runtime/expert_backend.py
@@ -21,6 +21,9 @@ class ExpertBackend(nn.Module):
         - Experts must always receive the same set of \*args and \*\*kwargs and produce output tensors of same type
         - All \*args, \*\*kwargs and outputs must be **tensors** where 0-th dimension represents to batch size
         - We recommend using experts that are ~invariant to the order in which they process batches
+        - Using randomness (e.g. Dropout) leads to different samples at forward and backward. If you want to ensure consistency,
+            you should register these samples as model outputs, so that they are sent back to the client.
+            See hivemind.utils.custom_layers.DeterministicDropout for an example
 
     :param opt: torch optimizer to be applied on every backward call
     :param args_schema: description of positional arguments to expert.forward, list of BatchTensorProto
@@ -65,7 +68,8 @@ class ExpertBackend(nn.Module):
 
            It should return gradients w.r.t. inputs that follow ``nested_flatten(self.outputs_schema)``;
 
-           .. todo state - we recommend stateless but you can save state if you want. disable batchnorm track running stats
+           TODO we handle layer states (e.g. batchnorm stats) incorrectly, updating them twice.
+           For now, either register all buffers as outputs or avoid stateful experts
 
         """
         args, kwargs = nested_pack(inputs, structure=self.forward_schema)
@@ -89,7 +93,7 @@ class ExpertBackend(nn.Module):
            Runtime doesn't guarantee that backward will be performed in the same order and for the same data
            as forward, so we recommend stateless backward pass that re-runs expert forward pass inside backward.
 
-           .. todo state, randomness, etc
+           TODO correct state handling (see forward)
 
            Please make sure to call ``ExpertBackend.apply_gradients`` **within** this method, otherwise the expert will not train
         """
