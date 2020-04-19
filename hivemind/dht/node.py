bd2fd0f8bd6bd86f180ccc9d81cc9021b0990e63
justheuristic <justheuristic@gmail.com>
2020-04-19 03:07:57 +0300

    move TODOs to PR

--- hivemind/dht/node.py
+++ hivemind/dht/node.py
@@ -19,16 +19,6 @@ class DHTNode:
         self.storage = storage or Storage()
         self.protocol = KademliaProtocol(self, bucket_size=beam_size)
 
-# changelog:
-# * less files (7 -> 4). mostly because chunks of code were replaced with python system libraries
-# * fixed a minor bug that resulted in wrong find_neighbors if k is greater than bucket size
-# * acyclic class dependencies: RoutingTable no longer calls KademliaProtocol, KademliaProtocol no longer calls DHTNode
-# * DHT node id is now just an integer with helper methods
-# * typing and docstrings for (hopefully) all public classes and methods
-# * updated to newer asyncio: ensure_future -> create_task, etc
-# * our implementaton no longer supports saving/loading a persistent state
-# * our implementaton no longer supports pylint
-# * large-scale tests for routing (10^4 nodes)
 
 # TODO bmuller's kademlia updated node's bucket:
 # * on every rpc_find_node - for the node that is searched for
@@ -36,9 +26,3 @@ class DHTNode:
 # * on every refresh table - for lonely_buckets
 # * on save_state - for bootstrappable neighbors, some reason
 # * on server.get/set/set_digest - for a bucket that contains key
-
-# implementation differences
-# (1) if a new node is rejected by RoutingTable (bucket is full and could NOT be split),
-#   bmuller's implementation would ping the first node in that bucket to *maybe* remove it if it doesn't respond
-#   our implementation
-# (2)
