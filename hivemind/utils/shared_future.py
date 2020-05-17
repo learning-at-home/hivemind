d6824f91d6fe159cc88de525267d8d9ee6b36a89
Max Ryabinin <mryabinin0@gmail.com>
2020-05-18 01:14:47 +0300

    AsyncConnection

--- hivemind/utils/shared_future.py
+++ hivemind/utils/shared_future.py
@@ -56,9 +56,8 @@ class SharedFuture(Future):
     def set_result(self, result):
         try:
             self.state, self._result = self.STATE_FINISHED, result
-            buf = PytorchSerializer.dumps((self.STATE_FINISHED, result))
             print(f'Sending {buf[:50]}')
-            self.connection.send(buf)
+            self.connection.send((self.STATE_FINISHED, result))
             return True
         except BrokenPipeError:
             return False
