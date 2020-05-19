c35b49769216ca756076fb0883c294aec4c73234
Max Ryabinin <mryabinin0@gmail.com>
2020-05-19 14:35:34 +0300

    Increase timeout

--- hivemind/server/connection_handler.py
+++ hivemind/server/connection_handler.py
@@ -4,14 +4,14 @@ import multiprocessing as mp
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 from uuid import uuid4
 import signal
-
+from functools import partial
 from hivemind.utils import PytorchSerializer
 
 
-def shutdown(executor):
+def ask_exit(signame, loop, executor):
+    print("got signal %s: exit" % signame)
     executor.shutdown()
-    for task in asyncio.Task.all_tasks():
-        task.cancel()
+    loop.stop()
 
 
 logger = logging.getLogger(__name__)
@@ -30,7 +30,10 @@ class ConnectionHandler(mp.Process):
         # self.loop.set_default_executor(ThreadPoolExecutor(1000))
 
         self.executor = ProcessPoolExecutor(self.conn_handler_processes, mp_context=mp.get_context('spawn'))
-        self.loop.add_signal_handler(signal.SIGTERM, shutdown, self.executor)
+        for signame in {'SIGINT', 'SIGTERM'}:
+            self.loop.add_signal_handler(
+                getattr(signal, signame),
+                partial(ask_exit, signame, self.loop, self.executor))
         just_read = lambda reader, writer: just_read_fn(reader, writer, self.executor, self.experts)
         start_server_fn = asyncio.start_server(just_read, *self.addr)
         self.loop.run_until_complete(start_server_fn)
