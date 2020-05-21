06d3dae78be1536f51070392f352f1edc4cda440
Vsevolod-pl <vsevolod-pl@yandex.ru>
2020-05-21 14:47:08 +0300

    Fixing cache cleaning for LocalStorage

--- hivemind/dht/protocol.py
+++ hivemind/dht/protocol.py
@@ -157,7 +157,7 @@ class LocalStorage:
 
     def remove_outdated_cache(self):
         while self.cache_heap and (self.cache_heap[0][0] < time.monotonic()
-                                   or len(self.cache_heap) >= self.maxsize):
+                                   or len(self.cache_heap) > self.maxsize):
             heap_entry = heapq.heappop(self.cache_heap)
             key = heap_entry[1]
             if self.cache_key_to_heap[key] == heap_entry:
