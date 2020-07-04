333fefc3dce4cf765ad0883ae4cc4ada4bec883f
justheuristic <justheuristic@gmail.com>
2020-07-05 02:39:44 +0300

    docstring

--- hivemind/dht/__init__.py
+++ hivemind/dht/__init__.py
@@ -1,13 +1,16 @@
 """
-This sub-module implements a node in a Kademlia-based DHT. The code is organized as follows:
+This is a Distributed Hash Table optimized for rapidly accessing a lot of lightweight metadata.
+Hivemind DHT is based on Kademlia [1] with added support for improved bulk store/get operations and caching.
+
+The code is organized as follows:
 
  * **class DHT (__init__.py)** - high-level class for model training. Runs DHTNode in a background process.
  * **class DHTNode (node.py)** - an asyncio implementation of dht server, stores AND gets keys.
  * **class DHTProtocol (protocol.py)** - an RPC protocol to request data from dht nodes.
  * **async def traverse_dht (traverse.py)** - a search algorithm that crawls DHT peers.
 
-The code in this module is a modified version of Kademlia DHT  https://github.com/bmuller/kademlia
-Brian, if you're reading this: THANK YOU! you're awesome :)
+- [1] Maymounkov P., Mazieres D. (2002) Kademlia: A Peer-to-Peer Information System Based on the XOR Metric.
+- [2] https://github.com/bmuller/kademlia , Brian, if you're reading this: THANK YOU! you're awesome :)
 """
 import asyncio
 import ctypes
