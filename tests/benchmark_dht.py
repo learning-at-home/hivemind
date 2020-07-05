812c496c22cdfd97fe939b94d477798733d956ef
Vsevolod-pl and Yozh <vsevolod-pl@yandex.ru>
2020-07-05 15:42:10 +0300

    make benchmark_dht into a function

--- tests/benchmark_dht.py
+++ tests/benchmark_dht.py
@@ -14,6 +14,70 @@ def random_endpoint() -> Tuple[str, int]:
             f"{random.randint(0, 256)}.{random.randint(0, 256)}", random.randint(0, 65535))
 
 
+def benchmark_dht(num_peers: int, initial_peers: int, num_experts: int, expert_batch_size: int, random_seed: int,
+                  wait_after_request: float, wait_before_read: float, wait_timeout: float, expiration_time: float):
+    old_expiration_time, hivemind.DHT.EXPIRATION = hivemind.DHT.EXPIRATION, expiration_time
+    random.seed(random_seed)
+
+    print("Creating peers...")
+    peers = []
+    for _ in trange(num_peers):
+        neighbors = [f'0.0.0.0:{node.port}' for node in random.sample(peers, min(initial_peers, len(peers)))]
+        peer = hivemind.DHT(*neighbors, start=True, wait_timeout=wait_timeout, listen_on=f'0.0.0.0:*')
+        peers.append(peer)
+
+    store_peer, get_peer = peers[-2:]
+
+    expert_uids = list(set(f"expert.{random.randint(0, 999)}.{random.randint(0, 999)}.{random.randint(0, 999)}"
+                           for _ in range(num_experts)))
+    print(f"Sampled {len(expert_uids)} unique ids (after deduplication)")
+    random.shuffle(expert_uids)
+
+    print(f"Storing peers to dht in batches of {expert_batch_size}...")
+    successful_stores = total_stores = total_store_time = 0
+    benchmark_started = time.perf_counter()
+    endpoints = []
+
+    for start in trange(0, num_experts, expert_batch_size):
+        store_start = time.perf_counter()
+        endpoints.append(random_endpoint())
+        success_list = store_peer.declare_experts(expert_uids[start: start + expert_batch_size], *endpoints[-1])
+        total_store_time += time.perf_counter() - store_start
+
+        total_stores += len(success_list)
+        successful_stores += sum(success_list)
+        time.sleep(wait_after_request)
+
+    print(f"Store success rate: {successful_stores / total_stores * 100:.1f}% ({successful_stores} / {total_stores})")
+    print(f"Mean store time: {total_store_time / total_stores:.5}, Total: {total_store_time:.5}")
+    time.sleep(wait_before_read)
+
+    if time.perf_counter() - benchmark_started > expiration_time:
+        warn("Warning: all keys expired before benchmark started getting them. Consider increasing expiration_time")
+
+    successful_gets = total_get_time = 0
+
+    for start in trange(0, len(expert_uids), expert_batch_size):
+        get_start = time.perf_counter()
+        get_result = get_peer.get_experts(expert_uids[start: start + expert_batch_size])
+        total_get_time += time.perf_counter() - get_start
+
+        for i, expert in enumerate(get_result):
+            if expert is not None and expert.uid == expert_uids[start + i] \
+                    and (expert.host, expert.port) == endpoints[start // expert_batch_size]:
+                successful_gets += 1
+
+    if time.perf_counter() - benchmark_started > expiration_time:
+        warn("Warning: keys expired midway during get requests. If that is not desired, increase expiration_time param")
+
+    print(f"Get success rate: {successful_gets / len(expert_uids) * 100:.1f} ({successful_gets} / {len(expert_uids)})")
+    print(f"Mean get time: {total_get_time / len(expert_uids):.5f}, Total: {total_get_time:.5f}")
+
+    alive_peers = [peer.is_alive() for peer in peers]
+    print(f"Node survival rate: {len(alive_peers) / len(peers) * 100:.3f}%")
+    hivemind.DHT.EXPIRATION = old_expiration_time
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--num_peers', type=int, default=32, required=False)
@@ -26,67 +90,9 @@ if __name__ == "__main__":
     parser.add_argument('--wait_timeout', type=float, default=5, required=False)
     parser.add_argument('--random_seed', type=int, default=random.randint(1, 1000))
     parser.add_argument('--increase_file_limit', action="store_true")
-    args = parser.parse_args()
+    args = vars(parser.parse_args())
 
-    if args.increase_file_limit:
+    if args.pop('increase_file_limit', False):
         increase_file_limit()
 
-    random.seed(args.random_seed)
-    hivemind.DHT.EXPIRATION = args.expiration_time
-
-    print("Creating peers...")
-    peers = []
-    for _ in trange(args.num_peers):
-        neighbors = [f'0.0.0.0:{node.port}' for node in random.sample(peers, min(args.initial_peers, len(peers)))]
-        peer = hivemind.DHT(*neighbors, start=True, wait_timeout=args.wait_timeout, listen_on=f'0.0.0.0:*')
-        peers.append(peer)
-
-    store_peer, get_peer = peers[-2:]
-
-    expert_uids = list(set(f"expert.{random.randint(0, 999)}.{random.randint(0, 999)}.{random.randint(0, 999)}"
-                           for _ in range(args.num_experts)))
-    print(f"Sampled {len(expert_uids)} unique ids (after deduplication)")
-    random.shuffle(expert_uids)
-
-    print(f"Storing peers to dht in batches of {args.expert_batch_size}...")
-    successful_stores = total_stores = total_store_time = 0
-    benchmark_started = time.perf_counter()
-    endpoints = []
-
-    for start in trange(0, args.num_experts, args.expert_batch_size):
-        store_start = time.perf_counter()
-        endpoints.append(random_endpoint())
-        success_list = store_peer.declare_experts(expert_uids[start: start + args.expert_batch_size], *endpoints[-1])
-        total_store_time += time.perf_counter() - store_start
-
-        total_stores += len(success_list)
-        successful_stores += sum(success_list)
-        time.sleep(args.wait_after_request)
-
-    print(f"Store success rate: {successful_stores / total_stores * 100:.1f}% ({successful_stores} / {total_stores})")
-    print(f"Mean store time: {total_store_time / total_stores:.5}, Total: {total_store_time:.5}")
-    time.sleep(args.wait_before_read)
-
-    if time.perf_counter() - benchmark_started > args.expiration_time:
-        warn("Warning: all keys expired before benchmark started getting them. Consider increasing expiration_time")
-
-    successful_gets = total_get_time = 0
-
-    for start in trange(0, len(expert_uids), args.expert_batch_size):
-        get_start = time.perf_counter()
-        get_result = get_peer.get_experts(expert_uids[start: start + args.expert_batch_size])
-        total_get_time += time.perf_counter() - get_start
-
-        for i, expert in enumerate(get_result):
-            if expert is not None and expert.uid == expert_uids[start + i] \
-                    and (expert.host, expert.port) == endpoints[start // args.expert_batch_size]:
-                successful_gets += 1
-
-    if time.perf_counter() - benchmark_started > args.expiration_time:
-        warn("Warning: keys expired midway during get requests. If that is not desired, increase expiration_time param")
-
-    print(f"Get success rate: {successful_gets / len(expert_uids) * 100:.1f} ({successful_gets} / {len(expert_uids)})")
-    print(f"Mean get time: {total_get_time / len(expert_uids):.5f}, Total: {total_get_time:.5f}")
-
-    alive_peers = [peer.is_alive() for peer in peers]
-    print(f"Node survival rate: {len(alive_peers) / len(peers) * 100:.3f}%")
+    benchmark_dht(**args)
