b40515d0b840b4ce0542e60521bfdb8f851e3c59
justheuristic <justheuristic@gmail.com>
2020-07-09 06:04:44 +0300

    create stub in a lazy manner

--- hivemind/client/expert.py
+++ hivemind/client/expert.py
@@ -11,7 +11,8 @@ from torch.autograd.function import once_differentiable
 from ..utils import nested_flatten, DUMMY, nested_pack, nested_compare, compile_grpc, serialize_torch_tensor, \
     deserialize_torch_tensor
 
-with open(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'server', 'connection_handler.proto')) as f_proto:
+with open(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'server',
+                       'connection_handler.proto')) as f_proto:
     runtime_pb2, runtime_grpc = compile_grpc(f_proto.read())
 
 
@@ -31,20 +32,29 @@ class RemoteExpert(nn.Module):
     def __init__(self, uid, host='127.0.0.1', port=8080):
         super().__init__()
         self.uid, self.host, self.port = uid, host, port
-        self.channel = grpc.insecure_channel(f'{self.host}:{self.port}', options=[
-            ('grpc.max_send_message_length', 50 * 1024 * 1024),
-            ('grpc.max_receive_message_length', 50 * 1024 * 1024)
-        ])
-        self.stub = runtime_grpc.ConnectionHandlerStub(self.channel)
+        self._channel, self._stub = None, None
         self._info = None
 
+    @property
+    def stub(self):
+        if self._channel is None:
+            self._channel = grpc.insecure_channel(f'{self.host}:{self.port}', options=[
+                ('grpc.max_send_message_length', 50 * 1024 * 1024),
+                ('grpc.max_receive_message_length', 50 * 1024 * 1024)
+            ])
+        if self._stub is None:
+            self._stub = runtime_grpc.ConnectionHandlerStub(self._channel)
+        return self._stub
+
     def __del__(self):
-        self.channel.close()
+        if self.channel is not None:
+            self.channel.close()
 
     def forward(self, *args, **kwargs):
         """ Call RemoteExpert for the specified inputs and return its output(s). Compatible with pytorch.autograd. """
         assert len(kwargs) == len(self.info['keyword_names']), f"Keyword args should be {self.info['keyword_names']}"
         kwargs = {key: kwargs[key] for key in self.info['keyword_names']}
+
         # Note: we put keyword arguments in the same order as on a server to prevent f(a=1, b=2) != f(b=2, a=1) errors
 
         forward_inputs = (args, kwargs)
@@ -52,7 +62,8 @@ class RemoteExpert(nn.Module):
         if not nested_compare(forward_inputs, self.info['forward_schema']):
             raise TypeError(f"Inputs do not match expert input schema. Did you pass the right number of parameters?")
 
-        flat_outputs = _RemoteModuleCall.apply(DUMMY, self.uid, self.host, self.port, self.stub, *nested_flatten(forward_inputs))
+        flat_outputs = _RemoteModuleCall.apply(DUMMY, self.uid, self.host, self.port, self.stub,
+                                               *nested_flatten(forward_inputs))
         # Note: we send DUMMY to prevent torch from excluding expert from backward if no other inputs require grad
         return nested_pack(flat_outputs, structure=self.info['outputs_schema'])
 
@@ -78,7 +89,8 @@ class _RemoteModuleCall(torch.autograd.Function):
         ctx.uid, ctx.host, ctx.port, ctx.stub = uid, host, port, stub
         ctx.save_for_backward(*inputs)
 
-        outputs = stub.forward(runtime_pb2.ExpertRequest(uid=ctx.uid, tensors=[serialize_torch_tensor(tensor) for tensor in inputs]))
+        outputs = stub.forward(
+            runtime_pb2.ExpertRequest(uid=ctx.uid, tensors=[serialize_torch_tensor(tensor) for tensor in inputs]))
 
         deserialized_outputs = [deserialize_torch_tensor(tensor) for tensor in outputs.tensors]
 
