99659a6fa297fb0a74dfd95599798e29072057dc
Vsevolod-pl <vsevolod-pl@yandex.ru>
2020-07-09 11:31:38 +0300

    Moved assert from function to main code

--- tests/test_expert_training.py
+++ tests/test_expert_training.py
@@ -1,8 +1,5 @@
 #%env CUDA_VISIBLE_DEVICES=
-import random
 import argparse
-from tqdm import tqdm_notebook
-import matplotlib.pyplot as plt
 import numpy as np
 
 import torch
@@ -57,10 +54,11 @@ def test_training(port:int):
             opt.step()
         train_pred = model(X_train).argmax(dim=1)
         accuracy = np.count_nonzero(train_pred == y_train)/y_train.shape[0]
-        assert accuracy >= 0.9, "too small accuracy: {accuracy}"
+        return accuracy
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', type=int, default=1376, required=False)
     args = vars(parser.parse_args())
-    test_training(args.port)
+    accuracy = test_training(args.port)
+    assert accuracy >= 0.9, "too small accuracy: {accuracy}"
