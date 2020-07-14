66d5e21678f64783dcccdad025fbd09908089c17
justheuristic <justheuristic@gmail.com>
2020-07-14 05:49:21 +0300

    instantiate executor in each process to avoid os locks

--- hivemind/utils/threading.py
+++ hivemind/utils/threading.py
@@ -3,12 +3,15 @@ from concurrent.futures import Future, as_completed, TimeoutError, ThreadPoolExe
 import time
 from typing import Optional, List
 
-GLOBAL_EXECUTOR = ThreadPoolExecutor(max_workers=os.environ.get("HIVEMIND_THREADS", float('inf')))
+EXECUTOR_PID, GLOBAL_EXECUTOR = None, None
 
 
 def run_in_background(func: callable, *args, **kwargs) -> Future:
     """ run func(*args, **kwargs) in background and return Future for its outputs """
-
+    global EXECUTOR_PID, GLOBAL_EXECUTOR
+    if os.getpid() != EXECUTOR_PID:
+        GLOBAL_EXECUTOR = ThreadPoolExecutor(max_workers=os.environ.get("HIVEMIND_THREADS", float('inf')))
+        EXECUTOR_PID = os.getpid()
     return GLOBAL_EXECUTOR.submit(func, *args, **kwargs)
 
 
