e6031d78b00e7fcf005a1410f4f262a0a88d429d
Max Ryabinin <mryabinin0@gmail.com>
2020-08-23 10:56:59 +0300

    Refactor custom build_ext into install and develop

--- setup.py
+++ setup.py
@@ -5,30 +5,20 @@ import re
 
 import grpc_tools.protoc
 from pkg_resources import parse_requirements
-from setuptools import setup
-from setuptools.command.build_ext import build_ext
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-with open('requirements.txt') as requirements_file:
-    install_requires = [str(requirement) for requirement in parse_requirements(requirements_file)]
-
-# loading version from setup.py
-with codecs.open(os.path.join(here, 'hivemind/__init__.py'), encoding='utf-8') as init_file:
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", init_file.read(), re.M)
-    version_string = version_match.group(1)
+from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from setuptools.command.install import install
 
 
-class ProtoCompileBuild(build_ext):
+def proto_compile(command_class):
+    orig_run = command_class.run
+
     def run(self):
-        super().run()
-
         cli_args = ['grpc_tools.protoc',
                     '--proto_path=hivemind/proto', '--python_out=hivemind/proto',
                     '--grpc_python_out=hivemind/proto'] + glob.glob('hivemind/proto/*.proto')
 
         code = grpc_tools.protoc.main(cli_args)
-
         if code:  # hint: if you get this error in jupyter, run in console for richer error message
             raise ValueError(f"{' '.join(cli_args)} finished with exit code {code}")
 
@@ -40,17 +30,44 @@ class ProtoCompileBuild(build_ext):
                 file.write(re.sub(r'\n(import .+_pb2.*)', 'from . \\1', code))
                 file.truncate()
 
+        orig_run(self)
+
+    command_class.run = run
+    return command_class
+
+
+@proto_compile
+class ProtoCompileInstall(install):
+    pass
+
+
+@proto_compile
+class ProtoCompileDevelop(develop):
+    pass
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open('requirements.txt') as requirements_file:
+    install_requires = [str(requirement) for requirement in parse_requirements(requirements_file)]
+
+# loading version from setup.py
+with codecs.open(os.path.join(here, 'hivemind/__init__.py'), encoding='utf-8') as init_file:
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", init_file.read(), re.M)
+    version_string = version_match.group(1)
 
 setup(
     name='hivemind',
     version=version_string,
-    cmdclass={'build_ext': ProtoCompileBuild},
+    cmdclass={'install': ProtoCompileInstall, 'develop': ProtoCompileDevelop},
     description='',
     long_description='',
     author='Learning@home authors',
     author_email='mryabinin@hse.ru',
     url="https://github.com/learning-at-home/hivemind",
-    packages=['hivemind'],
+    packages=find_packages(exclude=['tests']),
+    package_data={'hivemind': ['proto/*']},
+    include_package_data=True,
     license='MIT',
     install_requires=install_requires,
     classifiers=[
