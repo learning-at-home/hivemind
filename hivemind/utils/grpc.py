50a7535537fc27f9d15e4a2e999198d75d9c6a33
Vsevolod-pl <vsevolod-pl@yandex.ru>
2020-10-01 03:25:00 +0300

    Changed dtype of compressed float32 tensor to compressed_float32

--- hivemind/utils/grpc.py
+++ hivemind/utils/grpc.py
@@ -29,7 +29,7 @@ def serialize_torch_tensor(tensor: torch.Tensor, compression_type=CompressionTyp
             compression=compression_type,
             buffer=data,
             size=tensor.shape,
-            dtype='compressed_float16',
+            dtype='compressed_float32',
             requires_grad=tensor.requires_grad)
     else:
         array = tensor.numpy()
