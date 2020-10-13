8c9088c2bc2a9e49a9c60407c527bdbe52386a2c
justheuristic <justheuristic@gmail.com>
2020-10-13 09:05:23 +0300

    sphinx

--- hivemind/client/moe.py
+++ hivemind/client/moe.py
@@ -31,7 +31,7 @@ class RemoteMixtureOfExperts(nn.Module):
     :param in_features: common input size for experts and gating function
     :param grid_size: dimensions that form expert uid (see below)
     :param uid_prefix: common prefix for all expert uids
-     expert uid follows the pattern {uid_prefix}.{0...grid_size[0]}.{0...grid_size[1]}...{0...grid_size[-1]}
+    :note: expert uid follows the pattern {uid_prefix}.{0...grid_size[0]}.{0...grid_size[1]}...{0...grid_size[-1]}
     :param dht: DHT where the experts reside
     :param k_best: queries this many experts with highest scores
     :param k_min: makes sure at least this many experts returned output
