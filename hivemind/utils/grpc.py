1771a372a25d4b162beb95f75f965c8972e6964f
justheuristic <justheuristic@gmail.com>
2020-11-17 20:50:49 +0300

    Update hivemind/utils/grpc.py
    Co-authored-by: Max Ryabinin <mryabinin0@gmail.com>

--- hivemind/utils/grpc.py
+++ hivemind/utils/grpc.py
@@ -109,7 +109,7 @@ class ChannelCache(TimedStorage[ChannelInfo, Tuple[Union[grpc.Channel, grpc.aio.
             return namespace.insecure_channel(target, options=options, compression=compression)
         else:
             logger.debug(f"Creating secure {namespace} channel with credentials '{channel_credentials}', "
-                         + f"options '{options}' and compression '{compression}'")
+                         f"options '{options}' and compression '{compression}'")
             return namespace.secure_channel(target, credentials=channel_credentials,
                                             options=options, compression=compression)
 
