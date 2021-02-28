00b1688239010c097a9cdb177bee10f885d2707f
Alexey Bukhtiyarov <a.bukhtiyarov@yandex.ru>
2021-02-28 23:00:59 +0300

    Python3.7 notions

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Topic :: Scientific/Engineering',
