ed2a8a34c0d6067ffc7d5e55613c1df65f70c3ab
justheuristic <justheuristic@gmail.com>
2021-03-16 21:11:02 +0300

    simplify qq_encode

--- hivemind/utils/grpc.py
+++ hivemind/utils/grpc.py
@@ -163,8 +163,8 @@ class ChannelCache(TimedStorage[ChannelInfo, Tuple[Union[grpc.Channel, grpc.aio.
 
 def quantile_encode_torch_approx(weight, n_bits: int):
     n_bins = 2 ** n_bits
-    borders = torch.as_tensor(quantile_qq_approximation(weight.numpy(), n_bins + 1)[:-1])
-    quant_weight = torch.clamp_(torch.bucketize(weight, borders) - 1, 0, n_bins - 1)
+    borders = torch.as_tensor(quantile_qq_approximation(weight.numpy(), n_bins + 1)[1:-1])
+    quant_weight = torch.clamp_(torch.bucketize(weight, borders), 0, n_bins - 1)
     bin_sums = torch.zeros(n_bins).scatter_add_(0, quant_weight.flatten(), weight.flatten())
     bin_counts = torch.clamp_min_(torch.bincount(quant_weight.flatten(), minlength=n_bins), 1)
     lookup = bin_sums / bin_counts
