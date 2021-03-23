732fcf8bd9929ceae1552c567a1ede737754e635
justheuristic <justheuristic@gmail.com>
2021-03-23 23:58:20 +0300

    switch dht_handler to external declare_experts

--- hivemind/dht/__init__.py
+++ hivemind/dht/__init__.py
@@ -17,46 +17,20 @@ import asyncio
 import ctypes
 import heapq
 import multiprocessing as mp
-import re
 from collections import deque
 from concurrent.futures import ThreadPoolExecutor
-from typing import List, Tuple, Optional, Sequence, Union, Dict, Deque, NamedTuple, Iterator, Set, Callable, Awaitable, \
-    Any, TypeVar
+from typing import List, Tuple, Optional, Sequence, Union, Dict, Deque, Iterator, Set, Callable, Awaitable, TypeVar
 
 from hivemind.client import RemoteExpert
 from hivemind.dht.node import DHTNode, DHTID, DHTExpiration
 from hivemind.dht.routing import get_dht_time, DHTValue, DHTKey, Subkey
-from hivemind.utils import MPFuture, Endpoint, Hostname, get_logger, switch_to_uvloop, strip_port, ValueWithExpiration, \
-    await_cancelled
+from hivemind.utils.networking import Hostname, Endpoint, strip_port
+from hivemind.utils import MPFuture, get_logger, switch_to_uvloop, ValueWithExpiration, await_cancelled
+from hivemind.client.expert_uid import * #TODO remove
 
 logger = get_logger(__name__)
 
 ReturnType = TypeVar('ReturnType')
-
-ExpertUID, ExpertPrefix, Coordinate, Score = str, str, int, float
-UidEndpoint = NamedTuple("UidEndpoint", [('uid', ExpertUID), ('endpoint', Endpoint)])
-UID_DELIMITER = '.'  # when declaring experts, DHT store all prefixes of that expert's uid, split over this prefix
-FLAT_EXPERT = -1  # grid prefix reserved for storing 1d expert uids. Used to speed up find_best_experts in 1d case.
-UID_PATTERN = re.compile('^(([^.])+)([.](?:[0]|([1-9]([0-9]*))))+$')  # e.g. ffn_expert.98.76.54 - prefix + some dims
-PREFIX_PATTERN = re.compile('^(([^.])+)([.](?:[0]|([1-9]([0-9]*))))*[.]$')  # e.g. expert. or ffn.45. (ends with ".")
-#  formally, prefixes = {uid.split(UID_DELIMITER)[:length] for length in range(1, uid.count(UID_DELIMITER) + 2)}
-
-
-def is_valid_uid(maybe_uid: str) -> bool:
-    """ An uid must contain a string expert type, followed by one or more .-separated numeric indices """
-    return bool(UID_PATTERN.fullmatch(maybe_uid))
-
-
-def is_valid_prefix(maybe_prefix: str) -> bool:
-    """ An uid prefix must contain a string expert type, followed by optional numeric indices and a trailing period """
-    return bool(PREFIX_PATTERN.fullmatch(maybe_prefix))
-
-
-def split_uid(uid_or_prefix: Union[ExpertUID, ExpertPrefix]) -> Tuple[ExpertPrefix, Coordinate]:
-    """ Separate an expert UID or prefix into a new ExpertPrefix and integer for the last coordinate """
-    uid_or_prefix = uid_or_prefix.rstrip(UID_DELIMITER)
-    pivot = uid_or_prefix.rindex(UID_DELIMITER) + 1
-    return uid_or_prefix[:pivot], int(uid_or_prefix[pivot:])
 
 
 class DHT(mp.Process):
@@ -319,6 +293,7 @@ class DHT(mp.Process):
         :param timeout: waits for the procedure to finish for up to this long, None means wait indefinitely
         :returns: if wait, returns store status for every key (True = store succeeded, False = store rejected)
         """
+        assert timeout is None
         assert not isinstance(uids, str), "Please send a list / tuple of expert uids."
         for uid in uids:
             assert is_valid_uid(uid), f"{uid} is not a valid expert uid. All uids must follow {UID_PATTERN.pattern}"
