d61649122b1435763f2a2a2e61203739d7138e28
justheuristic <justheuristic@gmail.com>
2021-03-24 06:13:34 +0300

    pep8

--- tests/test_dht_experts.py
+++ tests/test_dht_experts.py
@@ -104,7 +104,7 @@ def test_dht_single_node():
     assert initial_beam[2][:2] == (0.0, 'expert.3.')
 
     with pytest.raises(AssertionError):
-        beam_search = MoEBeamSearcher(node, '')
+        beam_search = MoEBeamSearcher(node, 'expert.1.ffn')
 
     with pytest.raises(AssertionError):
         beam_search.get_active_successors(['e.1.2.', 'e.2', 'e.4.5.'])
