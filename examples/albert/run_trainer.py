2dbc6ccd9e8d9b84d5bbee09696bffd016b243e2
Alexey Bukhtiyarov <a.bukhtiyarov@yandex.ru>
2021-04-14 18:18:39 +0300

    Fix opt

--- examples/albert/run_trainer.py
+++ examples/albert/run_trainer.py
@@ -226,7 +226,7 @@ def main():
         eval_dataset=tokenized_datasets["validation"] if training_args.do_eval else None,
         tokenizer=tokenizer,
         data_collator=data_collator,
-        optimizers=(collaborative_optimizer, NoOpScheduler()),
+        optimizers=(collaborative_optimizer, NoOpScheduler(collaborative_optimizer)),
         callbacks=[CustomLoggingCallback(dht, collaborative_optimizer, collaboration_args, uuid.uuid4().hex)]
     )
 
