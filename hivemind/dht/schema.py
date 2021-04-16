02bedfb698ccb92ee237fe6fc599310aa6089087
Alexander Borzunov <hxrussia@gmail.com>
2021-04-16 22:35:40 +0300

    Support both str and bytes in the Cerberus `regex` rule

--- hivemind/dht/schema.py
+++ hivemind/dht/schema.py
@@ -1,4 +1,5 @@
-from typing import Any, Dict
+import re
+from typing import Any, Dict, Union
 
 import cerberus
 
@@ -12,11 +13,38 @@ logger = get_logger(__name__)
 
 
 class SchemaValidator(RecordValidatorBase):
+    """
+    Restricts specified DHT keys to match a Cerberus schema.
+    This allows to enforce types, min/max values, require a subkey to contain a public key, etc.
+    """
+
+    class _ExtendedCerberusValidator(cerberus.Validator):
+        """
+        By default, the Cerberus `regex` rule does not work with patterns for `bytes`.
+        We extend it to support both `str` and `bytes`.
+        The schema in the method's docstring describes the rule parameter.
+        """
+
+        def _validate_regex(self, pattern: Union[str, bytes], field: Any, value: Any):
+            """ {'anyof': [{'type': 'string'}, {'type': 'binary'}]} """
+
+            if type(pattern) != type(value):
+                self._error(field,
+                    f"Pattern type {type(pattern)} and value type {type(value)} don't match")
+                return
+            if re.fullmatch(pattern, value) is None:
+                self._error(field, cerberus.errors.REGEX_MISMATCH)
+
     def __init__(self, schema: Dict[DHTKey, Any]):
+        """
+        :param schema: The Cerberus validation schema.
+          See https://docs.python-cerberus.org/en/stable/validation-rules.html
+        """
+
         schema = {DHTID.generate(source=key).to_bytes(): value
                   for key, value in schema.items()}
 
-        self._validator = cerberus.Validator(schema)
+        self._validator = self._ExtendedCerberusValidator(schema)
         self._validator.allow_unknown = True
 
     def validate(self, record: DHTRecord) -> bool:
--- tests/test_dht_schema.py
+++ tests/test_dht_schema.py
@@ -3,28 +3,53 @@ import pytest
 from hivemind.dht import get_dht_time
 from hivemind.dht.node import DHTNode, LOCALHOST
 from hivemind.dht.schema import SchemaValidator
-from hivemind.dht.validation import DHTRecord
 
 
 @pytest.mark.forked
 @pytest.mark.asyncio
 async def test_schema_validator():
-    # See https://docs.python-cerberus.org/en/stable/validation-rules.html#type
     validator = SchemaValidator({
         b'experiment_name': {'type': 'binary'},
-        b'logs': {'type': 'dict',
-                  'keysrules': {'type': 'binary'},
-                  'valuesrules': {'type': 'binary'}},
         b'n_batches': {'type': 'dict',
-                       'keysrules': {'type': 'string', 'regex': r'\[owner:.+\]'},
+                       'keysrules': {'type': 'binary'},
                        'valuesrules': {'type': 'integer', 'min': 0}},
+        b'signed_data': {'type': 'dict',
+                         'keysrules': {'type': 'binary', 'regex': rb'^.*\[owner:.+\].*$'}},
     })
 
     alice = await DHTNode.create(record_validator=validator)
     bob = await DHTNode.create(
         record_validator=validator, initial_peers=[f"{LOCALHOST}:{alice.port}"])
 
+    # test 1: Unknown keys are allowed
+    assert await bob.store(b'unknown_key', b'foo_bar', get_dht_time() + 10)
+    assert (await alice.get(b'unknown_key', latest=True)).value == b'foo_bar'
+
+    # test 2: Regular value (bytes) expected
     assert await bob.store(b'experiment_name', b'foo_bar', get_dht_time() + 10)
     assert not await bob.store(b'experiment_name', 666, get_dht_time() + 10)
     assert not await bob.store(b'experiment_name', b'foo_bar', get_dht_time() + 10,
                                subkey=b'subkey')
+    assert (await alice.get(b'experiment_name', latest=True)).value == b'foo_bar'
+
+    # test 3: Dictionary (bytes -> int) expected
+    assert await bob.store(b'n_batches', 777, get_dht_time() + 10, subkey=b'uid1')
+    assert await bob.store(b'n_batches', 778, get_dht_time() + 10, subkey=b'uid2')
+    assert not await bob.store(b'n_batches', -666, get_dht_time() + 10, subkey=b'uid3')
+    assert not await bob.store(b'n_batches', 666, get_dht_time() + 10)
+    assert not await bob.store(b'n_batches', b'not_integer', get_dht_time() + 10, subkey=b'uid1')
+    assert not await bob.store(b'n_batches', 666, get_dht_time() + 10, subkey=666)
+    dictionary = (await alice.get(b'n_batches', latest=True)).value
+    assert (len(dictionary) == 2 and
+            dictionary[b'uid1'].value == 777 and
+            dictionary[b'uid2'].value == 778)
+
+    # test 4: Subkeys expected to contain a public key
+    # (so hivemind.dht.crypto.RSASignatureValidator would require a signature)
+    assert await bob.store(b'signed_data', b'foo_bar', get_dht_time() + 10,
+                           subkey=b'uid[owner:public-key]')
+    assert not await bob.store(b'signed_data', b'foo_bar', get_dht_time() + 10,
+                               subkey=b'uid-without-public-key')
+    dictionary = (await alice.get(b'signed_data', latest=True)).value
+    assert (len(dictionary) == 1 and
+            dictionary[b'uid[owner:public-key]'].value == b'foo_bar')
