3290a27d3aba961f203fa90e0551c3ed12bbc20a
Max Ryabinin <mryabinin0@gmail.com>
2021-04-19 03:13:00 +0300

    Reformat test_custom_expert.py

--- tests/test_custom_expert.py
+++ tests/test_custom_expert.py
@@ -1,19 +1,17 @@
 import os
-import pytest
-from typing import Optional
 
+import pytest
 import torch
 
-import hivemind
 from hivemind import RemoteExpert, background_server
 
-@pytest.mark.forked
-def test_custom_expert(port: Optional[int] = None, hid_dim=16):
-    with background_server(
-        expert_cls='perceptron', num_experts=2, device='cpu',
-        hidden_dim=hid_dim, num_handlers=2, no_dht=True,
-        custom_module_path=os.path.join(os.path.dirname(__file__), 'custom_networks.py')) as (server_endpoint, _):
 
+@pytest.mark.forked
+def test_custom_expert(hid_dim=16):
+    with background_server(
+            expert_cls='perceptron', num_experts=2, device='cpu',
+            hidden_dim=hid_dim, num_handlers=2, no_dht=True,
+            custom_module_path=os.path.join(os.path.dirname(__file__), 'custom_networks.py')) as (server_endpoint, _):
         expert0 = RemoteExpert('expert.0', server_endpoint)
         expert1 = RemoteExpert('expert.1', server_endpoint)
 
@@ -28,18 +26,19 @@ def test_custom_expert(port: Optional[int] = None, hid_dim=16):
             loss = output1.sum()
             loss.backward()
 
-@pytest.mark.forked
-def test_multihead_expert(port: Optional[int] = None, hid_dim=16):
-    with background_server(
-        expert_cls='multihead', num_experts=2, device='cpu',
-        hidden_dim=hid_dim, num_handlers=2, no_dht=True,
-        custom_module_path=os.path.join(os.path.dirname(__file__), 'custom_networks.py')) as (server_endpoint, _):
 
+@pytest.mark.forked
+def test_multihead_expert(hid_dim=16):
+    with background_server(
+            expert_cls='multihead', num_experts=2, device='cpu',
+            hidden_dim=hid_dim, num_handlers=2, no_dht=True,
+            custom_module_path=os.path.join(os.path.dirname(__file__), 'custom_networks.py')) as (server_endpoint, _):
         expert0 = RemoteExpert('expert.0', server_endpoint)
         expert1 = RemoteExpert('expert.1', server_endpoint)
 
         for batch_size in (1, 4):
-            batch = (torch.randn(batch_size, hid_dim), torch.randn(batch_size, 2 * hid_dim), torch.randn(batch_size, 3 * hid_dim))
+            batch = (torch.randn(batch_size, hid_dim), torch.randn(batch_size, 2 * hid_dim),
+                     torch.randn(batch_size, 3 * hid_dim))
 
             output0 = expert0(*batch)
             output1 = expert1(*batch)
