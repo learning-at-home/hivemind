cb8fad04eeb160277e44ef24f60ddc6c48d20002
Roman Zhytar <sir.roma2012@gmail.com>
2021-04-23 23:27:18 +0300

    Format

--- hivemind/optim/adaptive.py
+++ hivemind/optim/adaptive.py
@@ -12,6 +12,7 @@ class CollaborativeAdaptiveOptimizer(CollaborativeOptimizer):
     :param average_opt_statistics: average optimizer statistics with corresponding names in statedict
     :param kwargs: options for CollaborativeOptimizer
     """
+
     def __init__(self, average_opt_statistics: Sequence[str], **kwargs):
         self.average_opt_statistics = average_opt_statistics
         super().__init__(self, **kwargs)
@@ -20,6 +21,4 @@ class CollaborativeAdaptiveOptimizer(CollaborativeOptimizer):
         return TrainingAverager(self.opt, dht=self.dht, average_parameters=True, average_gradients=False,
                                 average_opt_statistics=self.average_opt_statistics,
                                 prefix=f"{self.prefix}_averaging", allreduce_timeout=self.averaging_timeout,
-                                listen=not self.client_mode, **kwargs
-                                )
-
+                                listen=not self.client_mode, **kwargs)
