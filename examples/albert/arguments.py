cf713ac3fe6efe95f3007106f41a5d183c5af498
Alexey Bukhtiyarov <a.bukhtiyarov@yandex.ru>
2021-04-29 16:03:55 +0300

    Refactoring

---
+++ examples/albert/arguments.py
@@ -0,0 +1,149 @@
+from typing import Optional, List
+from dataclasses import dataclass, field
+import uuid
+
+from transformers import TrainingArguments
+
+
+@dataclass
+class BaseTrainingArguments:
+    # Primary parameters
+    experiment_prefix: str = field(metadata={
+        "help": "A unique 'name' of this experiment, used to store metadata on the DHT"})
+    initial_peers: List[str] = field(
+        default_factory=list,
+        metadata={"help": "One or more peers (comma-separated) that will welcome you into the collaboration"}
+    )
+    averaging_expiration: float = field(
+        default=5.0,
+        metadata={"help": "Averaging group will wait for stragglers for at most this many seconds"}
+    )
+    averaging_timeout: float = field(
+        default=30.0,
+        metadata={"help": "Give up on averaging step after this many seconds"}
+    )
+    target_batch_size: int = field(
+        default=4096,
+        metadata={"help": "Perform optimizer step after all peers collectively accumulate this many samples"}
+    )
+    client_mode: bool = field(
+        default=False,
+        metadata={"help": "Of True, runs training without incoming connections, in a firewall-compatible mode"}
+    )
+
+    # Optional tweaks
+    target_group_size: int = field(
+        default=256,
+        metadata={"help": "Maximum group size for all-reduce"}
+    )
+    metadata_expiration: float = field(
+        default=30,
+        metadata={"help": "Peer's metadata will be removed if not updated in this many seconds"}
+    )
+    dht_listen_on: str = field(
+        default="[::]:*",
+        metadata={"help": "Network interface used for incoming DHT communication. Default: all ipv6"}
+    )
+    listen_on: str = field(
+        default="[::]:*",
+        metadata={"help": "Network interface used for incoming averager communication. Default: all ipv6"}
+    )
+    batch_size_lead: int = field(
+        default=0,
+        metadata={"help": "Optional: begin looking for group in advance, this many samples before target_batch_size"}
+    )
+    compression: str = field(
+        default="FLOAT16",
+        metadata={"help": "Use this compression when averaging parameters/gradients"}
+    )
+    min_refresh_period: float = field(
+        default=0.5,
+        metadata={"help": "Wait for at least this many seconds before fetching new collaboration state"}
+    )
+    max_refresh_period: float = field(
+        default=30,
+        metadata={"help": "Wait for at most this many seconds before fetching new collaboration state"}
+    )
+    default_refresh_period: float = field(
+        default=3,
+        metadata={"help": "Attempt to fetch collaboration state every this often until successful"}
+    )
+    expected_drift_peers: float = field(
+        default=3,
+        metadata={"help": "Trainer assumes that this many new peers can join per step"}
+    )
+    expected_drift_rate: float = field(
+        default=0.2,
+        metadata={"help": "Trainer assumes that this fraction of current size can join per step"}
+    )
+    bandwidth: float = field(
+        default=100.0,
+        metadata={"help": "Available network bandwidth, in mbps (used for load balancing in all-reduce)"}
+    )
+    performance_ema_alpha: float = field(
+        default=0.1,
+        metadata={"help": "Uses this alpha for moving average estimate of samples per second"}
+    )
+
+
+@dataclass
+class CollaborationArguments(BaseTrainingArguments):
+    trainer_uuid: str = field(
+        default=uuid.uuid4().hex,
+        metadata={"help": "this peer's name - used when publishing metadata to DHT, default = random"}
+    )
+    statistics_expiration: float = field(
+        default=600,
+        metadata={"help": "statistics will be removed if not updated in this many seconds"}
+    )
+    endpoint: Optional[str] = field(
+        default=None,
+        metadata={"help": "this node's IP for inbound connections, used when running from behind a proxy"}
+    )
+
+
+@dataclass
+class DatasetArguments:
+    dataset_path: Optional[str] = field(
+        default='./data/albert_tokenized_wikitext',
+        metadata={"help": "Path to the tokenized dataset"}
+    )
+    tokenizer_path: Optional[str] = field(
+        default='./data/tokenizer',
+        metadata={"help": "Path to the tokenizer"}
+    )
+    config_path: Optional[str] = field(
+        default='https://s3.amazonaws.com/models.huggingface.co/bert/albert-large-v2-config.json',
+        metadata={"help": "Path to the model config"}
+    )
+    cache_dir: Optional[str] = field(
+        default='./data',
+        metadata={"help": "Path to the cache"}
+    )
+
+
+@dataclass
+class AlbertTrainingArguments(TrainingArguments):
+    dataloader_num_workers: int = 4
+    per_device_train_batch_size: int = 4
+    per_device_eval_batch_size: int = 4
+    gradient_accumulation_steps: int = 2
+    seq_length: int = 512
+
+    max_steps: int = 1_000_000  # Albert is actually ready after 125000 steps
+    learning_rate: float = 0.00176
+    warmup_steps: int = 5000
+    adam_epsilon: float = 1e-6
+    weight_decay: float = 0.01
+    max_grad_norm: float = 1.0
+    clamp_value: float = 10000.0
+
+    fp16: bool = True
+    fp16_opt_level: str = 'O2'
+    do_train: bool = True
+
+    logging_steps: int = 100
+    save_total_limit: int = 2
+    save_steps: int = 500
+
+    output_dir: str = './outputs'
--- examples/albert/run_first_peer.py
+++ examples/albert/run_first_peer.py
@@ -3,8 +3,8 @@
 import time
 import subprocess
 import wandb
-from dataclasses import dataclass, asdict
-from typing import Optional, List
+from dataclasses import dataclass, field, asdict
+from typing import Optional, Any, Dict
 from whatsmyip.providers import GoogleDnsProvider
 from whatsmyip.ip import get_ip
 
@@ -13,45 +13,58 @@ import torch
 from torch_optimizer import Lamb
 from transformers import AlbertForPreTraining, AlbertConfig, HfArgumentParser
 from hivemind.utils.logging import get_logger
-
-from run_trainer import DatasetArguments, CollaborationArguments, \
-    get_model, get_optimizer_and_scheduler
+from arguments import BaseTrainingArguments
 
 
 logger = get_logger(__name__)
 
 
 @dataclass
-class CoordinatorArguments(CollaborationArguments):
-    address: Optional[str] = None  # this machine's network address. Use public IP for global experiments,
-    # local address for private runs
-    refresh_period: float = 30  # coordinator will fetch keys from DHT once in this many seconds
-    wandb_project: Optional[str] = None  # learning curves will be published there
-    save_checkpoint_step_interval: int = 5  # coordinator will load and save state from peers once every that many steps
-    upload_model_as: Optional[str] = None  # coordinator will upload the checkpoint to that HuggingFace repo
-    upload_interval: Optional[float] = None  # coordinator will upload model once in this many seconds
+class CoordinatorArguments(BaseTrainingArguments):
+    address: Optional[str] = field(
+        default=None,
+        metadata={"help": "This machine's network address. Use public IP for global experiments, "
+                          "local address for private runs"}
+    )
+    refresh_period: float = field(
+        default=30,
+        metadata={"help": "Coordinator will fetch keys from DHT once in this many seconds"}
+    )
+    wandb_project: Optional[str] = field(
+        default=None,
+        metadata={"help": "Learning curves will be published there"}
+    )
+    save_checkpoint_step_interval: int = field(
+        default=5,
+        metadata={"help": "Coordinator will load and save state from peers once every that many steps"}
+    )
+    model_config_path: str = field(
+        default='https://s3.amazonaws.com/models.huggingface.co/bert/albert-large-v2-config.json',
+        metadata={"help": "Path to the model config"}
+    )
+    repo_path: Optional[str] = field(
+        default=None,
+        metadata={"help":  "Path to HuggingFace repo in which coordinator will upload the model and optimizer states"}
+    )
+    upload_interval: Optional[float] = field(
+        default=None,
+        metadata={"help":  "Coordinator will upload model once in this many seconds"}
+    )
+
     # Note: You might want to have several initial peers so that if one dies,
     # new workers still can join the collaboration via alive initial peers' addresses.
     # Specify initial_peers argument for that purpose
 
 
 class CheckpointHandler:
-    def __init__(self, coordinator_args: CoordinatorArguments, dataset_args: DatasetArguments, dht: hivemind.DHT):
-        self.save_checkpoint_step_interval = coordinator_args.save_checkpoint_step_interval
-        self.upload_model_as = coordinator_args.upload_model_as
-        self.upload_interval = coordinator_args.upload_interval
+    def __init__(self, coordinator_args_dict: Dict[str, Any], dht: hivemind.DHT):
+        self.save_checkpoint_step_interval = coordinator_args_dict.pop('save_checkpoint_step_interval')
+        self.repo_path = coordinator_args_dict.pop('repo_path')
+        self.upload_interval = coordinator_args_dict.pop('upload_interval')
         self.previous_step = -1
 
-        config = AlbertConfig.from_pretrained(dataset_args.config_path)
+        config = AlbertConfig.from_pretrained(coordinator_args_dict.pop('model_config_path'))
         self.model = AlbertForPreTraining(config)
-
-        collaboration_args_dict = asdict(coordinator_args)
-        collaboration_args_dict.pop('address')
-        collaboration_args_dict.pop('refresh_period')
-        collaboration_args_dict.pop('wandb_project')
-        collaboration_args_dict.pop('save_checkpoint_step_interval')
-        collaboration_args_dict.pop('upload_model_as')
-        collaboration_args_dict.pop('upload_interval')
 
         no_decay = ["bias", "LayerNorm.weight"]
         optimizer_grouped_parameters = [
@@ -70,21 +83,15 @@ class CheckpointHandler:
             lr=0.00176, weight_decay=0.01, clamp_value=10000.0, debias=True,
         )
 
-        adjusted_target_batch_size = collaboration_args_dict.pop('target_batch_size') - \
-            collaboration_args_dict.pop('batch_size_lead')
-
-        collaboration_args_dict.pop('initial_peers')
-        collaboration_args_dict.pop('trainer_uuid')
-        collaboration_args_dict.pop('dht_listen_on')
-        collaboration_args_dict.pop('statistics_expiration')
-        collaboration_args_dict.pop('endpoint')
+        adjusted_target_batch_size = coordinator_args_dict.pop('target_batch_size') - \
+            coordinator_args_dict.pop('batch_size_lead')
 
         self.collaborative_optimizer = hivemind.CollaborativeOptimizer(
-            opt=opt, dht=dht, prefix=collaboration_args_dict.pop('experiment_prefix'),
-            compression_type=hivemind.utils.CompressionType.Value(collaboration_args_dict.pop('compression')),
-            throughput=collaboration_args_dict.pop('bandwidth'),
-            target_batch_size=adjusted_target_batch_size, client_mode=collaboration_args_dict.pop('client_mode'),
-            verbose=True, start=True, **collaboration_args_dict
+            opt=opt, dht=dht, prefix=coordinator_args_dict.pop('experiment_prefix'),
+            compression_type=hivemind.utils.CompressionType.Value(coordinator_args_dict.pop('compression')),
+            throughput=coordinator_args_dict.pop('bandwidth'),
+            target_batch_size=adjusted_target_batch_size, client_mode=coordinator_args_dict.pop('client_mode'),
+            verbose=True, start=True, **coordinator_args_dict
         )
         self.previous_timestamp = time.time()
 
@@ -101,58 +108,58 @@ class CheckpointHandler:
         self.previous_step = cur_step
 
     def is_time_to_upload(self):
-        if self.upload_model_as is None:
+        if self.repo_path is None:
             return False
         elif time.time() - self.previous_timestamp >= self.upload_interval:
             return True
         else:
             return False
 
-    def upload_checkpoint(self):
-        self.model.save_pretrained(self.upload_model_as)
-        torch.save(self.collaborative_optimizer.opt.state_dict(), f"{self.upload_model_as}/optimizer_state.pt")
+    def upload_checkpoint(self, current_loss):
+        self.model.save_pretrained(self.repo_path)
+        torch.save(self.collaborative_optimizer.opt.state_dict(), f"{self.repo_path}/optimizer_state.pt")
         self.previous_timestamp = time.time()
         try:
-            subprocess.run("git add --all".split(), check=True, cwd=self.upload_model_as)
-            subprocess.run("git commit -m Updating".split(), check=True, cwd=self.upload_model_as)
-            subprocess.run("git push".split(), check=True, cwd=self.upload_model_as)
-        except subprocess.CalledProcessError:
-            logger.warning("Error while uploading model")
+            subprocess.run("git add --all", shell=True, check=True, cwd=self.repo_path)
+            current_step = self.collaborative_optimizer.collaboration_state.optimizer_step
+            subprocess.run(f"git commit -m 'Step {current_step}, loss {round(current_loss, 3)}'", shell=True, check=True, cwd=self.repo_path)
+            subprocess.run("git push", shell=True, check=True, cwd=self.repo_path)
+        except subprocess.CalledProcessError as e:
+            logger.warning("Error while uploading model:", e.output)
 
 
 if __name__ == '__main__':
-    parser = HfArgumentParser((CoordinatorArguments, DatasetArguments))
-    coordinator_args, dataset_args = parser.parse_args_into_dataclasses()
+    parser = HfArgumentParser(CoordinatorArguments)
+    coordinator_args = parser.parse_args_into_dataclasses()
+    coordinator_args_dict = asdict(coordinator_args)
 
-    if coordinator_args.address is None:
+    coordinator_address = coordinator_args_dict.pop('address')
+    if coordinator_address is None:
         logger.warning("No address specified. Attempting to infer address from DNS.")
-        coordinator_args.address = get_ip(GoogleDnsProvider)
+        coordinator_address = get_ip(GoogleDnsProvider)
 
-    dht = hivemind.DHT(start=True, listen_on=coordinator_args.dht_listen_on,
-                       endpoint=f"{coordinator_args.address}:*", initial_peers=coordinator_args.initial_peers)
+    dht = hivemind.DHT(start=True, listen_on=coordinator_args_dict.pop('dht_listen_on'),
+                       endpoint=f"{coordinator_address}:*", initial_peers=coordinator_args_dict.pop('initial_peers'))
 
-    logger.info(f"Running DHT root at {coordinator_args.address}:{dht.port}")
+    logger.info(f"Running DHT root at {coordinator_address}:{dht.port}")
 
-    if coordinator_args.wandb_project is not None:
-        wandb.init(project=coordinator_args.wandb_project)
+    wandb_project = coordinator_args_dict.pop('wandb_project')
+    if wandb_project is not None:
+        wandb.init(project=wandb_project)
 
     current_step = 0
-    checkpoint_handler = CheckpointHandler(coordinator_args, dataset_args, dht)
+    experiment_prefix = coordinator_args_dict.pop('experiment_prefix')
+    refresh_period = coordinator_args_dict.pop('refresh_period')
+    checkpoint_handler = CheckpointHandler(coordinator_args_dict, dht)
 
     while True:
-        metrics_dict = dht.get(coordinator_args.experiment_prefix + '_metrics', latest=True)
+        metrics_dict = dht.get(experiment_prefix + '_metrics', latest=True)
         if metrics_dict is not None:
             metrics_dict = metrics_dict.value
             metrics = [metrics_dict[peer].value for peer in metrics_dict]
             latest_step = max(metrics)[0]
             if latest_step != current_step:
                 current_step = latest_step
-
-                if checkpoint_handler.is_time_to_save_state(current_step):
-                    checkpoint_handler.save_state(current_step)
-                    if checkpoint_handler.is_time_to_upload():
-                        checkpoint_handler.upload_checkpoint()
-
                 alive_peers = 0
                 num_batches = 0
                 sum_loss = 0
@@ -165,7 +172,7 @@ if __name__ == '__main__':
                     sum_perf += perf
                     num_samples += samples
                     sum_mini_steps += mini_steps
-                if coordinator_args.wandb_project is not None:
+                if wandb_project is not None:
                     wandb.log({
                         "loss": sum_loss / sum_mini_steps,
                         "alive peers": alive_peers,
@@ -173,5 +180,9 @@ if __name__ == '__main__':
                         "performance": sum_perf
                     })
                 logger.info(f"Step #{current_step}\tloss = {sum_loss / sum_mini_steps:.5f}")
+                if checkpoint_handler.is_time_to_save_state(current_step):
+                    checkpoint_handler.save_state(current_step)
+                    if checkpoint_handler.is_time_to_upload():
+                        checkpoint_handler.upload_checkpoint(sum_loss / sum_mini_steps)
         logger.debug("Peer is still alive...")
-        time.sleep(coordinator_args.refresh_period)
+        time.sleep(refresh_period)
