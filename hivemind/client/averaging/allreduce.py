d01e9ec7437f893b1d9d3fef210190c3924fc698
mponty <heapnhash@gmail.com>
2021-05-09 06:02:08 +0300

    MV: move some features from the original branch

--- hivemind/client/averaging/allreduce.py
+++ hivemind/client/averaging/allreduce.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import Sequence, Set, Dict, Tuple, Iterable, AsyncIterator, Any
+from typing import Sequence, Set, Dict, Tuple, List, Iterable, AsyncIterator, Any
 
 import grpc
 import torch
@@ -8,10 +8,145 @@ from hivemind.utils import Endpoint, get_logger, ChannelCache, anext
 from hivemind.utils import split_for_streaming, combine_from_streaming
 from hivemind.utils.compression import serialize_torch_tensor, deserialize_torch_tensor
 from hivemind.proto import averaging_pb2_grpc, runtime_pb2, averaging_pb2
+from collections import OrderedDict
 
 # flavour types
 GroupID = bytes
+TensorID = int
+CompressionType = Any
+Part = Tuple[torch.Tensor, ...]
 logger = get_logger(__name__)
+
+
+class TensorPartContainer:
+    def __init__(self, tensors: Sequence[torch.Tensor], part_sizes: Tuple[int, ...], endpoints: Sequence[Endpoint],
+                 compression_type: Sequence[CompressionType] = None):
+        assert len(endpoints) == len(part_sizes), "length of part_sizes mismatch with endpoints"
+        if compression_type is not None:
+            assert len(compression_type) == len(tensors), "length of compression type mismatch with number of tensors"
+
+        # Sort tensors descending by size
+        tensor_ids, tensors = zip(*sorted(enumerate(tensors), key=lambda idx_tensor: -idx_tensor[-1].numel()))
+        # Sort peers ascending by part_size
+        endpoints, part_sizes = zip(*sorted(zip(endpoints, part_sizes), key=lambda peer_size: peer_size[-1]))
+
+        parts, part_ids = self._split_into_parts(tensors, tensor_ids=tensor_ids, part_sizes=part_sizes)
+        self._orig_shapes = {idx: tensor.shape for idx, tensor in zip(tensor_ids, tensors)}
+
+        self._make_views(parts, part_ids, endpoints)
+        self.endpoints = tuple(endpoints)
+        if compression_type:
+            self._compression_type = compression_type
+        else:
+            self._compression_type = None
+
+    def _make_views(self, pieces, piece_ids, endpoints):
+        self._peer_tensor_id_view: Dict[Endpoint, Tuple[TensorID, ...]] = dict()
+        self._piece_view: Dict[Tuple[Endpoint, TensorID], torch.Tensor] = dict()
+
+        tensor_parts = []
+        for part_pieces, part_ids, endpoint in zip(pieces, piece_ids, endpoints):
+            self._peer_tensor_id_view[endpoint] = tuple(part_ids)
+
+            part_keys = zip([endpoint] * len(part_ids), part_ids)
+            tensor_parts.extend(zip(part_keys, part_pieces))
+
+        self._piece_view = dict(tensor_parts)
+
+    def get_piece(self, peer: Endpoint, tensor_id: int) -> torch.Tensor:
+        return self._piece_view[(peer, tensor_id)]
+
+    def get_part(self, peer: Endpoint) -> Part:
+        """Return peer part of tensor pieces. pieces ordered by ids"""
+        return tuple(self.get_piece(peer, idx) for idx in self._peer_tensor_id_view[peer])
+
+    def get_part_with_ids(self, peer: Endpoint) -> Tuple[Tuple[TensorID, ...], Part]:
+        """Return peer part of tensor pieces. pieces ordered by ids"""
+        return self._peer_tensor_id_view[peer], self.get_part(peer)
+
+    def set_piece(self, piece: torch.Tensor, peer: Endpoint, tensor_id: int):
+        assert self._piece_view[(peer, tensor_id)].shape == piece.shape, "piece shape mismatch"
+        assert self._piece_view[(peer, tensor_id)].dtype == piece.dtype, "piece dtype mismatch"
+        self._piece_view[(peer, tensor_id)] = piece
+
+    def set_part(self, peer_part: Part, peer: Endpoint, tensor_ids: List[TensorID] = None):
+        """pieces must be ordered by tensor id"""
+        if tensor_ids is None:
+            tensor_ids = self._peer_tensor_id_view[peer]
+        for tensor_id, tensor in zip(tensor_ids, peer_part):
+            self.set_piece(tensor, peer, tensor_id)
+
+    def get_shapes(self, peer: Endpoint) -> Tuple[torch.Size, ...]:
+        return tuple(piece.shape for piece in self.get_part(peer))
+
+    def get_dtypes(self, peer: Endpoint) -> Tuple[torch.dtype, ...]:
+        return tuple(piece.dtype for piece in self.get_part(peer))
+
+    def get_part_compression_type(self, peer: Endpoint) -> Tuple[CompressionType, ...]:
+        if self._compression_type is None:
+            return None
+        return tuple(self._compression_type[tensor_id] for tensor_id in self._peer_tensor_id_view[peer])
+
+    @property
+    def tensors(self) -> Sequence[torch.Tensor]:
+        part_keys, pieces = zip(*self._piece_view.items())
+        _, piece_ids = zip(*part_keys)
+
+        tensor_ids = sorted(self._orig_shapes.keys())
+        restored = self._restore_from_parts(piece_ids, pieces)
+        restored = [restored.get(idx, torch.tensor([])) for idx in tensor_ids]
+        shapes = [self._orig_shapes[idx] for idx in tensor_ids]
+
+        return list(map(torch.Tensor.reshape, restored, shapes))
+
+    @staticmethod
+    def _split_into_parts(tensors: Sequence[torch.Tensor],
+                          tensor_ids: Sequence[torch.Tensor],
+                          part_sizes: Tuple[int]) -> Tuple[Sequence[Part], Sequence[List[int]]]:
+        """ combines averaged_tensors into one tensor and splits them into equal pieces of size group_size """
+        tensors = tuple(map(torch.Tensor.flatten, tensors))
+        enumerated_tensors = zip(tensor_ids, tensors)
+        peer_parts, peer_part_ids = [], []
+
+        for part_size in part_sizes:
+            part, part_ids = [], []
+            accum_size = 0
+
+            for tensor_id, tensor in enumerated_tensors:
+                tensor_size = tensor.numel()
+
+                if accum_size + tensor_size <= part_size:
+                    part.append(tensor)
+                    part_ids.append(tensor_id)
+                    accum_size += tensor_size
+
+                    if accum_size == part_size:
+                        break
+                else:
+                    residue = part_size - accum_size
+                    shards = tensor[:residue], tensor[residue:]
+
+                    part.append(shards[0])
+                    part_ids.append(tensor_id)
+                    enumerated_tensors = iter([(tensor_id, shards[1])] + list(enumerated_tensors))
+                    break
+
+            part = tuple(part)
+            peer_parts.append(part)
+            peer_part_ids.append(part_ids)
+        return peer_parts, peer_part_ids
+
+    @staticmethod
+    def _restore_from_parts(piece_ids: Sequence[TensorID],
+                            pieces: Sequence[torch.Tensor]) -> Dict[TensorID, torch.Tensor]:
+        """ restores the original tensor shapes from pieces obtained by split_into_pieces """
+        restored, shapes = dict(), []
+        for tensor_id in piece_ids:
+            if tensor_id not in restored:
+                tensor = torch.cat([tensor for tid, tensor in zip(piece_ids, pieces) if tid == tensor_id])
+                restored[tensor_id] = tensor
+
+        return restored
 
 
 class AllReduceProtocol:
@@ -27,12 +162,14 @@ class AllReduceProtocol:
     """
 
     def __init__(self, *, group_id: GroupID, tensors: Sequence[torch.Tensor], endpoint: Endpoint,
-                 ordered_group_endpoints: Sequence[Endpoint], part_sizes: Tuple[int, ...], return_deltas: bool = False):
+                 ordered_group_endpoints: Sequence[Endpoint], part_sizes: Tuple[int, ...], return_deltas: bool = False,
+                 compression_type: Sequence[CompressionType] = None):
         assert endpoint in ordered_group_endpoints, "endpoint is not a part of the group"
         self.group_id, self.endpoint = group_id, endpoint
         self.ordered_group_endpoints, self.part_sizes = ordered_group_endpoints, part_sizes
         self.client_mode_endpoints = {endpoint for endpoint, part_size in zip(self.ordered_group_endpoints, part_sizes)
                                       if part_size == 0}
+        self.compression_type = compression_type
         self.local_tensor_parts = dict(zip(ordered_group_endpoints, split_into_parts(tensors, part_sizes)))
         self.tensor_shapes = tuple(tensor.shape for tensor in tensors)
         self.return_deltas = return_deltas
@@ -40,11 +177,13 @@ class AllReduceProtocol:
         self.accumulator = torch.zeros_like(self.local_tensor_parts[self.endpoint])
         self.denominator = 0.0  # number of peers added to accumulator or sum of their weights
         self.accumulated_from: Set[Endpoint] = set()  # peers that we have accumulated our part from
+        self.registered_from: Set[Endpoint] = set()  # peers that we have accumulated our part from
         self.averaged_part: asyncio.Future[torch.Tensor] = asyncio.Future()  # will be set to [accumulator / group size]
         self.averaged_tensor_parts: Dict[Endpoint, torch.Tensor] = {}  # averaged chunks from all peers will be put here
         self.future: asyncio.Future[Sequence[torch.Tensor]] = asyncio.Future()  # final result or exception
         for endpoint in self.client_mode_endpoints:
             self.averaged_tensor_parts[endpoint] = torch.tensor([])
+        self.registered_from.update(self.client_mode_endpoints)
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.endpoint}, group_size={self.group_size})"
@@ -53,13 +192,27 @@ class AllReduceProtocol:
         return self.future.__await__()
 
     def __contains__(self, endpoint: Endpoint):
-        return endpoint in self.local_tensor_parts
+        return endpoint in self.ordered_group_endpoints
 
     @property
     def group_size(self):
         return len(self.ordered_group_endpoints)
 
-    async def accumulate_part(self, source: Endpoint, remote_part: torch.Tensor, weight: float = 1.0) -> torch.Tensor:
+    def _accumulate_pieces(self, pieces: Part, weight: float = 1.0):
+        # TODO: mock it
+        tensor_ids, _ = self.local_tensor_parts.get_part_with_ids(self.endpoint)
+        assert len(pieces) == len(tensor_ids)
+
+        self.denominator += weight
+        for tensor_id, piece in zip(tensor_ids, pieces):
+            accumulator = self.accumulators[tensor_id]
+            accumulator.add_(piece, alpha=weight)
+
+            if len(self.accumulated_from) == len(self.ordered_group_endpoints):
+                accumulator.div_(self.denominator)
+
+    async def accumulate_part(self, source: Endpoint, remote_part: Part, weight: float = 1.0) \
+            -> Part:
         """ Add vector part to accumulator, wait for all other vectors to be added, then return the average part """
         assert not self.averaged_part.done(), f"already finished averaging part: {self.averaged_part}"
         assert not self.future.done(), f"already finished allreduce: {self.future}"
@@ -142,7 +295,36 @@ class AllReduceRunner(AllReduceProtocol, averaging_pb2_grpc.DecentralizedAveragi
     def _get_peer_stub(self, peer: Endpoint) -> averaging_pb2_grpc.DecentralizedAveragingStub:
         return ChannelCache.get_stub(peer, averaging_pb2_grpc.DecentralizedAveragingStub, aio=True)
 
-    async def _communicate_with_peer(self, peer_endpoint: Endpoint, local_part: torch.Tensor) -> torch.Tensor:
+    def _serialize_to_chunks(self, local_part: Part, compression_type: Sequence[CompressionType],
+                             chunk_size_bytes: int, header=False) -> Iterable[averaging_pb2.AveragingData]:
+        assert len(local_part) == len(compression_type)
+
+        for i, (tensor, compression) in enumerate(zip(local_part, compression_type)):
+            serialized_tensor = serialize_torch_tensor(tensor, compression, allow_inplace=False)
+            chunks = split_for_streaming(serialized_tensor, chunk_size_bytes)
+
+            code = averaging_pb2.PART_FOR_AVERAGING if i == 0 else averaging_pb2.TENSOR_SEP
+            if header:
+                yield averaging_pb2.AveragingData(code=code, group_id=self.group_id,
+                                                  endpoint=self.endpoint, tensor_part=next(chunks))
+            for chunk in chunks:
+                yield averaging_pb2.AveragingData(tensor_part=chunk)
+
+    def _deserialize_from_chunks(self, chunks: Iterable[averaging_pb2.AveragingData]) -> Part:
+        piece, averaged_pieces = [], []
+        for chunk in chunks:
+            if chunk.code == averaging_pb2.TENSOR_SEP:
+                averaged_pieces.append(piece)
+                piece = []
+            piece.append(chunk.tensor_part)
+        averaged_pieces.append(piece)
+
+        averaged_part = []
+        for piece in averaged_pieces:
+            averaged_part.append(deserialize_torch_tensor(combine_from_streaming(piece)))
+        return tuple(averaged_part)
+
+    async def _communicate_with_peer(self, peer_endpoint: Endpoint, local_part: Part) -> Part:
         """ Send a part of local tensors and metadata to a single peer, receive the average for that part of tensors """
         if peer_endpoint == self.endpoint:
             return await self.accumulate_part(self.endpoint, local_part, weight=self.peer_weights[self.endpoint])
@@ -204,7 +386,8 @@ class AllReduceRunner(AllReduceProtocol, averaging_pb2_grpc.DecentralizedAveragi
             raise AllreduceException(f"Could not deserialize tensor part from {source} for streaming {e}")
 
         averaged_part = await self.accumulate_part(source, tensor_part, weight=self.peer_weights[source])
-        serialized_tensor = serialize_torch_tensor(averaged_part - tensor_part, self.compression_type, allow_inplace=False)
+        serialized_tensor = serialize_torch_tensor(averaged_part - tensor_part, self.compression_type,
+                                                   allow_inplace=False)
         stream_chunks = tuple(split_for_streaming(serialized_tensor, self.chunk_size_bytes))
         return stream_chunks
 
