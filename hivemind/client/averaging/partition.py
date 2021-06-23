033509783fb07ed9c02fb1939d5a464b02472f5b
justheuristic <justheuristic@gmail.com>
2021-06-23 15:17:04 +0300

    rename chunks -> parts in all affected code

--- hivemind/client/averaging/partition.py
+++ hivemind/client/averaging/partition.py
@@ -1,5 +1,8 @@
+"""
+Auxiliary data structures for AllReduceProtocol and AllReduceRunner
+"""
 import asyncio
-from typing import Sequence, Awaitable, AsyncIterable
+from typing import Sequence, Awaitable, AsyncIterable, Tuple, Optional
 
 import torch
 import numpy as np
@@ -8,102 +11,165 @@ from hivemind.utils.compression import serialize_torch_tensor, deserialize_torch
 from collections import deque
 
 
-class TensorPartition:
+class TensorPartContainer:
     """
     Auxiliary data structure for averaging, responsible for splitting tensors into parts and re-assembling them.
     The class is designed to avoid excessive memory allocation and run all heavy computation in background
     """
 
-    def __init__(self, tensors: Sequence[torch.Tensor], part_sizes: Sequence[float],
+    def __init__(self, tensors: Sequence[torch.Tensor], target_size_by_peer: Sequence[float],
                  compression_type: runtime_pb2.CompressionType = runtime_pb2.CompressionType.NONE,
-                 chunk_size_bytes: int = 2 ** 20):
-        self.local_tensors, self.part_sizes, self.chunk_size_bytes = tensors, part_sizes, chunk_size_bytes
+                 part_size_bytes: int = 2 ** 20):
+        self.local_tensors, self.part_sizes, self.part_size_bytes = tensors, target_size_by_peer, part_size_bytes
+        self.group_size = len(target_size_by_peer)
         self.tensor_sizes = [tensor.numel() for tensor in tensors]
         self.compression_type = compression_type
         self.total_size = sum(self.tensor_sizes)
-        self._input_chunks_by_peer = [deque() for _ in range(self.num_peers)]
-        self._output_chunks_by_peer = [deque() for _ in range(self.num_peers)]
-        self._inputs_consumed_by_peer = [False for _ in range(self.num_peers)]
-        self._output_chunks_available = [asyncio.Event() for _ in range(self.num_peers)]
-        self._num_chunks_by_peer, self._num_chunks_by_tensor = [], []
+        self.num_parts_by_peer, self.num_parts_by_tensor = [], []
+        self._input_parts_by_peer = [deque() for _ in range(self.group_size)]
+        self._output_parts_by_peer = [deque() for _ in range(self.group_size)]
+        self._inputs_consumed_by_peer = [False for _ in range(self.group_size)]
+        self._output_part_available = [asyncio.Event() for _ in range(self.group_size)]
         self._outputs_consumed = False
 
-        # split chunks in proportion to part_sizes
+        # split tensor parts in proportion to target_size_by_peer
         current_length = 0
         current_peer_index = 0
-        pivots = np.cumsum(part_sizes) / np.sum(part_sizes) * self.total_size
+        pivots = np.cumsum(target_size_by_peer) / np.sum(target_size_by_peer) * self.total_size
         pivots = np.concatenate([pivots.astype(np.int64)[:-1], [self.total_size]])
 
         for tensor in self.local_tensors:
-            chunk_size_values = int(chunk_size_bytes / get_nbytes_per_value(tensor.dtype, self.compression_type))
-            tensor_chunks = tensor.detach().view(-1).split(chunk_size_values)
-            self._num_chunks_by_tensor.append(len(tensor_chunks))
-            for chunk in tensor_chunks:
-                if current_length + len(chunk) > pivots[current_peer_index]:
-                    # switch to next peer; if a chunk lands between parts of two or
-                    # more peers, assign that chunk to the peer with highest intersecton
+            part_size_values = int(part_size_bytes / get_nbytes_per_value(tensor.dtype, self.compression_type))
+            tensor_parts = tensor.detach().view(-1).split(part_size_values)
+            self.num_parts_by_tensor.append(len(tensor_parts))
+            for part in tensor_parts:
+                if current_length + len(part) > pivots[current_peer_index]:
+                    # switch to next peer; if a part lands between parts of two or
+                    # more peers, assign that part to the peer with highest intersection
                     prev_peer_index = current_peer_index
                     peer_intersections = [pivots[current_peer_index] - current_length]
-                    while current_length + len(chunk) > pivots[current_peer_index]:
+                    while current_length + len(part) > pivots[current_peer_index]:
                         current_peer_index += 1
-                        current_peer_chunk_end = min(current_length + len(chunk), pivots[current_peer_index])
-                        peer_intersections.append(current_peer_chunk_end - pivots[current_peer_index - 1])
+                        current_peer_part_end = min(current_length + len(part), pivots[current_peer_index])
+                        peer_intersections.append(current_peer_part_end - pivots[current_peer_index - 1])
                     assigned_peer_index = prev_peer_index + np.argmax(peer_intersections)
-                    self._input_chunks_by_peer[assigned_peer_index].append(chunk)
+                    self._input_parts_by_peer[assigned_peer_index].append(part)
                 else:
-                    self._input_chunks_by_peer[current_peer_index].append(chunk)
-                current_length += len(chunk)
+                    self._input_parts_by_peer[current_peer_index].append(part)
+                current_length += len(part)
         assert current_length == self.total_size
-        for current_peer_index in range(self.num_peers):
-            self._num_chunks_by_peer.append(len(self._input_chunks_by_peer[current_peer_index]))
-
-    @property
-    def num_peers(self) -> int:
-        return len(self.part_sizes)
+        for current_peer_index in range(self.group_size):
+            self.num_parts_by_peer.append(len(self._input_parts_by_peer[current_peer_index]))
 
     @torch.no_grad()
-    async def iterate_input_chunks(self, peer_index: int) -> runtime_pb2.Tensor:
-        """ get tensor chunks that should be averaged by a peer at a given index. Run serialization in background. """
-        assert not self._inputs_consumed_by_peer[peer_index], "input chunks of a given peer are already deallocated."
+    def get_raw_input_parts(self, peer_index: int) -> Tuple[torch.Tensor, ...]:
+        """ get non-serialized tensor parts for a peer at a given index """
+        assert not self._inputs_consumed_by_peer[peer_index], "input parts of a given peer are already deallocated."
         self._inputs_consumed_by_peer[peer_index] = True
-        if not self._input_chunks_by_peer[peer_index]:
+        input_parts = tuple(self._input_parts_by_peer[peer_index])
+        self._input_parts_by_peer[peer_index].clear()
+        return input_parts
+
+    @torch.no_grad()
+    async def iterate_input_parts_for(self, peer_index: int) -> runtime_pb2.Tensor:
+        """ iterate serialized tensor parts for a peer at a given index. Run serialization in background. """
+        assert not self._inputs_consumed_by_peer[peer_index], "input parts of a given peer are already deallocated."
+        self._inputs_consumed_by_peer[peer_index] = True
+        if not self._input_parts_by_peer[peer_index]:
             return
         loop = asyncio.get_event_loop()
 
-        def _serialize_next_chunk() -> Awaitable[runtime_pb2.Tensor]:
-            next_chunk = self._input_chunks_by_peer[peer_index].popleft()
-            return loop.run_in_executor(None, lambda: serialize_torch_tensor(next_chunk, self.compression_type))
+        def _serialize_next_part() -> Awaitable[runtime_pb2.Tensor]:
+            next_part = self._input_parts_by_peer[peer_index].popleft()
+            return loop.run_in_executor(None, lambda: serialize_torch_tensor(next_part, self.compression_type))
 
-        prefetch_next_chunk = _serialize_next_chunk()
-        for i in range(len(self._input_chunks_by_peer[peer_index])):
-            next_chunk = await prefetch_next_chunk
-            prefetch_next_chunk = _serialize_next_chunk()
-            yield next_chunk
-        yield await prefetch_next_chunk
+        prefetch_next_part = _serialize_next_part()
+        for i in range(len(self._input_parts_by_peer[peer_index])):
+            next_part = await prefetch_next_part
+            prefetch_next_part = _serialize_next_part()
+            yield next_part
+        yield await prefetch_next_part
 
-    def append_averaged_chunk(self, peer_index: int, chunk: runtime_pb2.Tensor):
-        """ register next-in-line chunk of results received from a given peer  """
-        self._output_chunks_by_peer[peer_index].append(
-            asyncio.get_event_loop().run_in_executor(None, deserialize_torch_tensor, chunk))
-        self._output_chunks_available[peer_index].set()
+    def append_averaged_part(self, peer_index: int, part: runtime_pb2.Tensor):
+        """ register next-in-line part of results received from a given peer  """
+        self._output_parts_by_peer[peer_index].append(
+            asyncio.get_event_loop().run_in_executor(None, deserialize_torch_tensor, part))
+        self._output_part_available[peer_index].set()
 
     async def iterate_output_tensors(self) -> AsyncIterable[torch.Tensor]:
         """ iterate over the outputs of averaging (whether they are average, delta or other aggregation result) """
         assert not self._outputs_consumed, "output tensors are already iterated and no longer available."
         self._outputs_consumed = True
-        peer_index = num_chunks_processed = 0
+        peer_index = num_parts_processed = 0
         for tensor_index in range(len(self.local_tensors)):
-            tensor_chunks = []
-            while len(tensor_chunks) < self._num_chunks_by_tensor[tensor_index]:
-                if num_chunks_processed >= self._num_chunks_by_peer[peer_index]:
-                    num_chunks_processed = 0
+            tensor_parts = []
+            while len(tensor_parts) < self.num_parts_by_tensor[tensor_index]:
+                if num_parts_processed >= self.num_parts_by_peer[peer_index]:
+                    num_parts_processed = 0
                     peer_index += 1
                     continue
-                if not self._output_chunks_by_peer[peer_index]:
-                    self._output_chunks_available[peer_index].clear()
-                    await self._output_chunks_available[peer_index].wait()
-                tensor_chunks.append(await self._output_chunks_by_peer[peer_index].popleft())
-                num_chunks_processed += 1
-            tensor = torch.cat(tensor_chunks)
-            del tensor_chunks
+                if not self._output_parts_by_peer[peer_index]:
+                    self._output_part_available[peer_index].clear()
+                    await self._output_part_available[peer_index].wait()
+                tensor_parts.append(await self._output_parts_by_peer[peer_index].popleft())
+                num_parts_processed += 1
+            tensor = torch.cat(tensor_parts)
+            del tensor_parts
             yield tensor.reshape(self.local_tensors[tensor_index].shape)
+
+
+class TensorPartReducer:
+    """
+    Auxiliary data structure responsible for running asynchronous all-reduce
+    :param local_parts: a sequence of local torch tensors that will be averaged with other peers
+    :param peer_index: index of local peer in a given all-reduce group
+    :param group_size: total number of peers in a given all-reduce group
+    :param inplace: if set to True, local_tensors can be modified in-place for reduce
+    """
+    next_part_index: int = 0  # index in local_parts of the part that should be loaded next
+    accumulator: torch.Tensor  # sum of current tensor part from group peers
+    denominator: float  # total weight accumulated from all peers for current part
+    current_part_accumulated_from: int  # number of peers from which the current part was accumulated (including self)
+
+    def __init__(self, local_parts: Sequence[torch.Tensor], peer_index: int, group_size: int,
+                 weights: Optional[Sequence[float]] = None, inplace: bool = False):
+        self.local_parts, self.peer_index, self.group_size, self.inplace = local_parts, peer_index, group_size, inplace
+        self.num_parts_accumulated = [0 for _ in range(group_size)]
+        self.weights = tuple(weights or (1 for _ in range(group_size)))
+        assert len(weights) == self.group_size
+
+    def prepare_next_part(self):
+        """ create averaging buffers for the part part in line, pre-populates with local tensor part """
+        next_local_part = self.local_parts[self.next_part_index]
+        self.accumulator = next_local_part if self.inplace else next_local_part.clone()
+        self.denominator = self.weights[self.peer_index]
+        self.num_parts_accumulated[self.peer_index] += 1
+        self.current_part_accumulated_from = 1
+
+    async def accumulate_part(self, peer_index, remote_part: torch.Tensor, weight: float = 1.0) -> torch.Tensor:
+        """ Add vector part to accumulator, wait for all other vectors to be added, then return the average part """
+        assert not self.averaged_part.done(), f"already finished averaging part: {self.averaged_part}"
+        assert not self.future.done(), f"already finished allreduce: {self.future}"
+        assert source in self.local_tensor_parts, "unexpected source, not a part of current group"
+        assert source not in self.current_part_accumulated_from, "duplicate source, already received that part"
+        assert self.peer_modes[
+                   self.endpoint] != AveragingMode.CLIENT, f"{self.endpoint} is in AveragingMode.client mode"
+        assert isinstance(weight, (int, float)) and weight > 0, "averaging weights must be a non-negative int/float"
+
+        logger.debug(f"{self} - accumulating tensor part from {source}")
+        self.accumulator.add_(remote_part, alpha=weight)
+        self.denominator += weight
+        self.current_part_accumulated_from.add(source)
+
+        assert len(self.current_part_accumulated_from) <= self.num_senders
+        if len(self.current_part_accumulated_from) == self.num_senders:
+            average_result = self.accumulator.div_(self.denominator)
+            self.averaged_part.set_result(average_result)
+
+            if self.peer_modes[self.endpoint] == AveragingMode.AUX:
+                self.future.set_result(None)  # auxiliary mode has finished averaging
+            else:
+                self.register_averaged_part(self.endpoint, average_result)
+
+        return await self.averaged_part
+
--- tests/test_allreduce.py
+++ tests/test_allreduce.py
@@ -6,7 +6,7 @@ from typing import Sequence
 import pytest
 import torch
 
-from hivemind.client.averaging.partition import TensorPartition
+from hivemind.client.averaging.partition import TensorPartContainer
 from hivemind.utils import serialize_torch_tensor, deserialize_torch_tensor
 from hivemind.proto.runtime_pb2 import CompressionType
 
@@ -21,18 +21,18 @@ async def test_partitioning():
     ]
 
     # note: this test does _not_ use parameterization to reuse sampled tensors
-    for chunk_size_bytes in 31337, 2 ** 23, 10 ** 10:
+    for part_size_bytes in 31337, 2 ** 23, 10 ** 10:
         for num_tensors in 1, 3, 5, len(all_tensors):
-            for part_sizes in [(0.25, 0.25, 0.25, 0.25), (0.333, 0.1667, 0.5003), (1.0, 0.0), [0.0, 0.4, 0.6, 0.0]]:
+            for weights in [(0.25, 0.25, 0.25, 0.25), (0.333, 0.1667, 0.5003), (1.0, 0.0), [0.0, 0.4, 0.6, 0.0]]:
                 tensors = random.choices(all_tensors, k=num_tensors)
-                partition = TensorPartition(tensors, part_sizes, chunk_size_bytes=chunk_size_bytes)
+                partition = TensorPartContainer(tensors, weights, part_size_bytes=part_size_bytes)
 
                 async def write_tensors():
-                    for i in range(partition.num_peers):
-                        async for chunk in partition.iterate_input_chunks(i):
-                            output_tensor = torch.sin(deserialize_torch_tensor(chunk))
-                            chunk = serialize_torch_tensor(output_tensor, chunk.compression)
-                            partition.append_averaged_chunk(i, chunk)
+                    for i in range(partition.group_size):
+                        async for part in partition.iterate_input_parts_for(i):
+                            output_tensor = torch.sin(deserialize_torch_tensor(part))
+                            part = serialize_torch_tensor(output_tensor, part.compression)
+                            partition.append_averaged_part(i, part)
                 asyncio.create_task(write_tensors())
                 tensor_index = 0
                 async for output_tensor in partition.iterate_output_tensors():
@@ -42,14 +42,14 @@ async def test_partitioning():
 
 @pytest.mark.parametrize("tensors", [[torch.zeros(0)], [torch.zeros(0), torch.zeros(0), torch.zeros(1)],
                                      [torch.zeros(0), torch.zeros(999), torch.zeros(0), torch.zeros(0)]])
-@pytest.mark.parametrize("part_sizes", [(0.33, 0.44, 0.23), (0.5, 0.5), (0.1, 0.0, 0.9), (1.0,), (0.1,) * 9])
+@pytest.mark.parametrize("weights", [(0.33, 0.44, 0.23), (0.5, 0.5), (0.1, 0.0, 0.9), (1.0,), (0.1,) * 9])
 @pytest.mark.forked
 @pytest.mark.asyncio
-async def test_partitioning_edge_cases(tensors: Sequence[torch.Tensor], part_sizes: Sequence[float]):
-    partition = TensorPartition(tensors, part_sizes, chunk_size_bytes=16)
-    for i in range(len(part_sizes)):
-        async for chunk in partition.iterate_input_chunks(i):
-            partition.append_averaged_chunk(i, chunk)
+async def test_partitioning_edge_cases(tensors: Sequence[torch.Tensor], weights: Sequence[float]):
+    partition = TensorPartContainer(tensors, weights, part_size_bytes=16)
+    for i in range(len(weights)):
+        async for part in partition.iterate_input_parts_for(i):
+            partition.append_averaged_part(i, part)
 
     tensor_index = 0
     async for output_tensor in partition.iterate_output_tensors():
@@ -63,15 +63,15 @@ async def test_partitioning_asynchronous():
     """ ensure that tensor partitioning does not interfere with asynchronous code """
     tensors = [torch.randn(2048, 2048), torch.randn(1024, 4096),
                torch.randn(4096, 1024), torch.randn(30_000, 1024)]
-    part_sizes = [0.4, 0.3, 0.2, 0.1]
+    weights = [0.4, 0.3, 0.2, 0.1]
 
-    partition = TensorPartition(tensors, part_sizes, compression_type=CompressionType.QUANTILE_8BIT)
+    partition = TensorPartContainer(tensors, weights, compression_type=CompressionType.QUANTILE_8BIT)
     read_started, read_finished = asyncio.Event(), asyncio.Event()
 
     async def write_tensors():
-        for i in range(partition.num_peers):
-            async for chunk in partition.iterate_input_chunks(i):
-                partition.append_averaged_chunk(i, chunk)
+        for i in range(partition.group_size):
+            async for part in partition.iterate_input_parts_for(i):
+                partition.append_averaged_part(i, part)
         assert read_started.is_set(), "partitioner should have started reading before it finished writing"
 
     async def read_tensors():
