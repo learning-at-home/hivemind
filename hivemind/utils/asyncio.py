ec1ee5ee9d699a8ad2f14d913b0c24eb91abbae0
justheuristic <justheuristic@gmail.com>
2021-06-24 20:53:00 +0300

    background map with janus

--- hivemind/utils/asyncio.py
+++ hivemind/utils/asyncio.py
@@ -1,6 +1,10 @@
 from typing import TypeVar, AsyncIterator, Union, AsyncIterable, Awaitable, Tuple
 import asyncio
+
+import janus
 import uvloop
+
+
 T = TypeVar('T')
 
 
@@ -27,6 +31,16 @@ async def aiter(*args: T) -> AsyncIterator[T]:
         yield arg
 
 
+async def azip(*iterables: AsyncIterable[T]) -> AsyncIterator[Tuple[T, ...]]:
+    """ equivalent of zip for asynchronous iterables """
+    iterators = [iterable.__aiter__() for iterable in iterables]
+    while True:
+        try:
+            yield tuple(await asyncio.gather(*(itr.__anext__() for itr in iterators)))
+        except StopAsyncIteration:
+            break
+
+
 async def achain(*async_iters: AsyncIterable[T]) -> AsyncIterator[T]:
     """ equivalent to chain(iter1, iter2, ...) for asynchronous iterators. """
     for aiter in async_iters:
@@ -39,6 +53,7 @@ async def aenumerate(aiterable: AsyncIterable[T]) -> AsyncIterable[Tuple[int, T]
     index = 0
     async for elem in aiterable:
         yield index, elem
+        index += 1
 
 
 async def await_cancelled(awaitable: Awaitable) -> bool:
@@ -49,3 +64,37 @@ async def await_cancelled(awaitable: Awaitable) -> bool:
         return True
     except BaseException:
         return False
+
+
+async def async_map(func: callable, *iterables: AsyncIterable, max_prefetch: int):
+    """ iterate from an async iterable in a background thread, yield results to async iterable """
+    assert max_prefetch > 0
+    inputs, outputs = janus.Queue(max_prefetch), janus.Queue(max_prefetch)
+
+    async def _put_inputs():
+        async for args in azip(*iterables):
+            await inputs.async_q.put(args)
+        await inputs.async_q.put(None)
+
+    def _thread():
+        try:
+            args = inputs.sync_q.get()
+            while args is not None:
+                outputs.sync_q.put(func(*args))
+                args = inputs.sync_q.get()
+            outputs.sync_q.put(None)
+        except BaseException as e:
+            outputs.sync_q.put(e)
+
+    asyncio.create_task(_put_inputs())
+    asyncio.get_event_loop().run_in_executor(None, _thread)
+
+    output = await outputs.async_q.get()
+    while output is not None:
+        if isinstance(output, Exception):
+            raise output
+        else:
+            yield output
+            output = await outputs.async_q.get()
+
+
