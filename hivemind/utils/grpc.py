fed179d2dfa5a3520e60e8202851a2e14e30e78b
justheuristic <justheuristic@gmail.com>
2021-06-28 16:23:54 +0300

    rollback

--- hivemind/utils/grpc.py
+++ hivemind/utils/grpc.py
@@ -161,16 +161,16 @@ class ChannelCache(TimedStorage[ChannelInfo, Tuple[Union[grpc.Channel, grpc.aio.
 STREAMING_CHUNK_SIZE_BYTES = 2 ** 16
 
 
-def split_for_streaming(serialized_tensor: runtime_pb2.Tensor) -> Iterator[runtime_pb2.Tensor]:
+def split_for_streaming(serialized_tensor: runtime_pb2.Tensor, chunk_size_bytes: int = STREAMING_CHUNK_SIZE_BYTES,
+                        ) -> Iterator[runtime_pb2.Tensor]:
     """ Split serialized_tensor into multiple chunks for gRPC streaming """
     buffer = memoryview(serialized_tensor.buffer)
-    num_chunks = len(range(0, len(buffer), STREAMING_CHUNK_SIZE_BYTES))
+    num_chunks = len(range(0, len(buffer), chunk_size_bytes))
     yield runtime_pb2.Tensor(
-        compression=serialized_tensor.compression, buffer=buffer[:STREAMING_CHUNK_SIZE_BYTES].tobytes(),
-        chunks=num_chunks, size=serialized_tensor.size, dtype=serialized_tensor.dtype,
-        requires_grad=serialized_tensor.requires_grad)
-    for chunk_start in range(STREAMING_CHUNK_SIZE_BYTES, len(buffer), STREAMING_CHUNK_SIZE_BYTES):
-        yield runtime_pb2.Tensor(buffer=buffer[chunk_start: chunk_start + STREAMING_CHUNK_SIZE_BYTES].tobytes())
+        compression=serialized_tensor.compression, buffer=buffer[:chunk_size_bytes].tobytes(), chunks=num_chunks,
+        size=serialized_tensor.size, dtype=serialized_tensor.dtype, requires_grad=serialized_tensor.requires_grad)
+    for chunk_start in range(chunk_size_bytes, len(buffer), chunk_size_bytes):
+        yield runtime_pb2.Tensor(buffer=buffer[chunk_start: chunk_start + chunk_size_bytes].tobytes())
 
 
 def combine_from_streaming(stream: Iterable[runtime_pb2.Tensor]) -> runtime_pb2.Tensor:
