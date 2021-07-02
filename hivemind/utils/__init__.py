3205df97e0b52a3bb54231e412f648bac7dea633
justheuristic <justheuristic@gmail.com>
2021-07-02 23:16:21 +0300

    isort

--- hivemind/utils/__init__.py
+++ hivemind/utils/__init__.py
@@ -1,6 +1,7 @@
 from hivemind.utils.asyncio import *
 from hivemind.utils.compression import serialize_torch_tensor, deserialize_torch_tensor
 from hivemind.utils.grpc import *
+from hivemind.utils.limits import *
 from hivemind.utils.logging import get_logger
 from hivemind.utils.mpfuture import *
 from hivemind.utils.nested import *
@@ -8,4 +9,3 @@ from hivemind.utils.networking import *
 from hivemind.utils.serializer import *
 from hivemind.utils.tensor_descr import *
 from hivemind.utils.timed_storage import *
-from hivemind.utils.limits import *
