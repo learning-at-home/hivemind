20a480a212cdba4411f8e0a8e3ed9d3ed2bd0043
justheuristic <justheuristic@gmail.com>
2021-07-03 01:51:30 +0300

    [internal changes to MPFuture] (#305)
    * test forever
    
    * spawn new syncmanager for each run
    
    * rollback before merge
    
    * docstr
    
    * review
    
    * type hint for callback
    
    Co-authored-by: Aleksandr Borzunov <hxrussia@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,23 @@
+import multiprocessing as mp
+
 import pytest
 import psutil
 
+from hivemind.utils.mpfuture import MPFuture
+
 
 @pytest.fixture(autouse=True, scope='session')
-def cleanup_children():
-    yield
-    for child in psutil.Process().children(recursive=True):
-        child.terminate()
+def cleanup_after_test():
+    """ reset shared memory manager for isolation, terminate any leftover processes after the test is finished """
+    old_values = MPFuture._global_mpfuture_senders, MPFuture._active_pid, MPFuture._active_futures
+    try:
+        with mp.managers.SyncManager() as manager:
+            MPFuture._global_mpfuture_senders = manager.dict()
+            MPFuture._active_pid = MPFuture._active_futures = None
+
+            yield
+
+        for child in psutil.Process().children(recursive=True):
+            child.terminate()
+    finally:
+        MPFuture._global_mpfuture_senders, MPFuture._active_pid, MPFuture._active_futures = old_values
