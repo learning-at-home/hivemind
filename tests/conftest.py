081890050901d119d11aff20bb269774c22b87fa
justheuristic <justheuristic@gmail.com>
2021-07-03 01:22:42 +0300

    spawn new syncmanager for each run

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,22 @@
+import multiprocessing as mp
+
 import pytest
 import psutil
 
+from hivemind.utils.mpfuture import MPFuture
+
 
 @pytest.fixture(autouse=True, scope='session')
-def cleanup_children():
-    yield
-    for child in psutil.Process().children(recursive=True):
-        child.terminate()
+def cleanup_after_test():
+    old_values = MPFuture.global_mpfuture_senders, MPFuture.active_pid, MPFuture.active_futures
+    try:
+        with mp.managers.SyncManager() as manager:
+            MPFuture.global_mpfuture_senders = manager.dict()
+            MPFuture.active_pid = MPFuture.active_futures = None
+
+            yield
+
+        for child in psutil.Process().children(recursive=True):
+            child.terminate()
+    finally:
+        MPFuture.global_mpfuture_senders, MPFuture.active_pid, MPFuture.active_futures = old_values
