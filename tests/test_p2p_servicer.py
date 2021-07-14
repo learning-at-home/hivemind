b2611c4aa499d6bd76c14ab82460bcd7549a8340
Aleksandr Borzunov <hxrussia@gmail.com>
2021-07-14 21:00:01 +0300

    Add stream-stream test

--- tests/test_p2p_servicer.py
+++ tests/test_p2p_servicer.py
@@ -68,3 +68,30 @@ async def test_unary_stream(server_client):
         assert item == test_pb2.TestResponse(number=i)
         i += 1
     assert i == 10
+
+
+@pytest.mark.asyncio
+async def test_stream_stream(server_client):
+    class ExampleServicer(ServicerBase):
+        async def rpc_powers(self, request: AsyncIterable[test_pb2.TestRequest],
+                             _: P2PContext) -> AsyncIterable[test_pb2.TestResponse]:
+            async for item in request:
+                yield test_pb2.TestResponse(number=item.number ** 2)
+                yield test_pb2.TestResponse(number=item.number ** 3)
+
+    server, client = server_client
+    servicer = ExampleServicer()
+    await servicer.add_p2p_handlers(server)
+    stub = servicer.get_stub(client, server.id)
+
+    async def generate_requests() -> AsyncIterable[test_pb2.TestRequest]:
+        for i in range(10):
+            yield test_pb2.TestRequest(number=i)
+
+    i = 0
+    async for item in stub.rpc_powers(generate_requests()):
+        if i % 2 == 0:
+            assert item == test_pb2.TestResponse(number=(i // 2) ** 2)
+        else:
+            assert item == test_pb2.TestResponse(number=(i // 2) ** 3)
+        i += 1
