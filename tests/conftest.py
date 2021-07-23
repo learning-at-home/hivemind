af412db08113ac6c3b98164fd853ddea7c3c481f
justheuristic <justheuristic@gmail.com>
2021-07-23 22:06:51 +0300

    test averaging only

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,6 +23,10 @@ def cleanup_children():
         for child in children:
             with suppress(psutil.NoSuchProcess):
                 child.terminate()
+        psutil.wait_procs(children, timeout=1)
+        for child in children:
+            with suppress(psutil.NoSuchProcess):
+                child.kill()
 
     # Broken code or killing of child processes may leave the MPFuture backend corrupted
     MPFuture.reset_backend()
