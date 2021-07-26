261f597fbaf529579b14e621b1a6857498b1ae6e
justheuristic <justheuristic@gmail.com>
2021-07-27 01:17:55 +0300

    unfail fast

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
 import gc
 from contextlib import suppress
+import multiprocessing as mp
 
 import psutil
 import pytest
 
+from hivemind.utils.mpfuture import MPFuture
 from hivemind.utils.logging import get_logger
 
 
@@ -26,3 +28,7 @@ def cleanup_children():
         for child in children:
             with suppress(psutil.NoSuchProcess):
                 child.kill()
+
+    MPFuture._active_pid = None
+    MPFuture._initialization_lock = mp.Lock()
+    MPFuture._update_lock = mp.Lock()
