cee03d81b32f8330a24d53c94281551f671bf07b
Alexander Borzunov <hxrussia@gmail.com>
2021-08-03 19:46:31 +0300

    Use find_open_port() instead of unused_tcp_port

--- tests/test_p2p_daemon.py
+++ tests/test_p2p_daemon.py
@@ -11,6 +11,7 @@ from multiaddr import Multiaddr
 
 from hivemind.p2p import P2P, P2PDaemonError, P2PHandlerError
 from hivemind.proto import dht_pb2
+from hivemind.utils.networking import find_open_port
 from hivemind.utils.serializer import MSGPackSerializer
 
 
@@ -34,10 +35,10 @@ async def test_daemon_killed_on_del():
 
 
 @pytest.mark.asyncio
-async def test_startup_error_message(unused_tcp_port: int):
+async def test_startup_error_message():
     with pytest.raises(P2PDaemonError, match=r"Failed to connect to bootstrap peers"):
         await P2P.create(
-            initial_peers=[f"/ip4/127.0.0.1/tcp/{unused_tcp_port}/p2p/QmdaK4LUeQaKhqSFPRu9N7MvXUEWDxWwtCvPrS444tCgd1"]
+            initial_peers=[f"/ip4/127.0.0.1/tcp/{find_open_port()}/p2p/QmdaK4LUeQaKhqSFPRu9N7MvXUEWDxWwtCvPrS444tCgd1"]
         )
 
     with pytest.raises(P2PDaemonError, match=r"Daemon failed to start in .+ seconds"):
