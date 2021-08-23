f4ce4c32e510a927644165cc3538ceed4ed28652
Denis Mazur <denismazur8@gmail.com>
2021-08-24 02:34:50 +0300

    bump p2p version

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ from setuptools import find_packages, setup
 from setuptools.command.build_py import build_py
 from setuptools.command.develop import develop
 
-P2PD_VERSION = "v0.3.3-updated-protocol"
-P2PD_CHECKSUM = "05cdef573bc608771c70910146a7b35f"
+P2PD_VERSION = "v0.3.4"
+P2PD_CHECKSUM = "194dca06116fdd36bc4b681d18f3b9cb"
 LIBP2P_TAR_URL = f"https://github.com/learning-at-home/go-libp2p-daemon/archive/refs/tags/{P2PD_VERSION}.tar.gz"
 
 here = os.path.abspath(os.path.dirname(__file__))
