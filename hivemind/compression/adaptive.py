90f6e2cc651174440a12b27fa4a9b1cad0a535d1
justheuristic <justheuristic@gmail.com>
2021-09-01 21:01:05 +0300

    role-adaptive compression

--- hivemind/compression/adaptive.py
+++ hivemind/compression/adaptive.py
@@ -3,7 +3,7 @@ from typing import Mapping, Union, Sequence
 import torch
 
 import hivemind
-from hivemind.compression.base import CompressionBase, CompressionInfo, UID
+from hivemind.compression.base import CompressionBase, UID, CompressionInfo, NoCompression, TensorRole
 from hivemind.proto import runtime_pb2
 
 
@@ -25,6 +25,30 @@ class SizeAdaptiveCompression(AdaptiveCompressionBase):
     def compress(self, tensor: torch.Tensor, info: CompressionInfo, allow_inplace: bool = False) -> runtime_pb2.Tensor:
         compression = self.small if info.descriptor.numel() > self.threshold else self.large
         return compression.compress(tensor, info=info, allow_inplace=allow_inplace)
+
+
+class RoleAdaptiveCompression(AdaptiveCompressionBase):
+    """Compress a tensor based on its role in training. Any non-specified compressions will use the "default" option"""
+
+    def __init__(self, *,
+                 activation: CompressionBase = None,
+                 parameter: CompressionBase = None,
+                 gradient: CompressionBase = None,
+                 optimizer: CompressionBase = None,
+                 default: CompressionBase = NoCompression()):
+        self.role_compressions = {
+            TensorRole.ACTIVATION: activation or default,
+            TensorRole.PARAMETER: parameter or default,
+            TensorRole.GRADIENT: gradient or default,
+            TensorRole.OPTIMIZER: optimizer or default,
+            TensorRole.UNSPECIFIED: default,
+        }
+
+    def estimate_compression_ratio(self, info: CompressionInfo) -> float:
+        return self.role_compressions[info.role].estimate_compression_ratio(info)
+
+    def compress(self, tensor: torch.Tensor, info: CompressionInfo, allow_inplace: bool = False) -> runtime_pb2.Tensor:
+        return self.role_compressions[info.role].compress(tensor, info=info, allow_inplace=allow_inplace)
 
 
 class PerTensorCompression(AdaptiveCompressionBase):
