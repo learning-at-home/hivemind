bf2695eadb289992ecb599f8fa6a246c085d7f4b
Alexander Borzunov <hxrussia@gmail.com>
2021-09-10 23:52:11 +0300

    isort

--- benchmarks/benchmark_averaging.py
+++ benchmarks/benchmark_averaging.py
@@ -7,9 +7,9 @@ import torch
 
 import hivemind
 from hivemind.proto import runtime_pb2
-from hivemind.utils.networking import LOCALHOST
 from hivemind.utils.limits import increase_file_limit
 from hivemind.utils.logging import get_logger, use_hivemind_log_handler
+from hivemind.utils.networking import LOCALHOST
 
 use_hivemind_log_handler("in_root_logger")
 logger = get_logger(__name__)
