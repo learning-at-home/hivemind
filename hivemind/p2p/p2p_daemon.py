9ea5e1430f69bccb8ca3959c99a03421c068f25a
justheuristic <justheuristic@gmail.com>
2021-09-13 20:40:47 +0300

    smaller chunks yet

--- hivemind/p2p/p2p_daemon.py
+++ hivemind/p2p/p2p_daemon.py
@@ -278,7 +278,7 @@ class P2P:
 
     @staticmethod
     async def send_protobuf(protobuf: Union[TOutputProtobuf, RPCError],
-                            writer: asyncio.StreamWriter, *, chunk_size: int = 2 ** 16) -> None:
+                            writer: asyncio.StreamWriter, *, chunk_size: int = 2 ** 14) -> None:
         if isinstance(protobuf, RPCError):
             writer.write(P2P.ERROR_MARKER)
         else:
