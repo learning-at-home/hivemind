30f7dde856613e4fd549abcb1e364bebca32840a
Denis Mazur <denismazur8@gmail.com>
2021-09-20 22:32:09 +0300

    refactor device data

--- hivemind/utils/tensor_descr.py
+++ hivemind/utils/tensor_descr.py
@@ -96,12 +96,12 @@ class BatchTensorDescriptor(TensorDescriptor):
         obj_dict["dtype"] = str(self.dtype) if self.dtype is not None else None
         obj_dict["layout"] = str(self.layout) if self.layout is not None else None
 
-        obj_dict.pop("device")
-        if self.device is not None:
-            obj_dict.update(
-                device_type=self.device.type,
-                device_index=self.device.index,
-            )
+        device = obj_dict.pop("device")
+        device_type, device_index = (device.index, device.type) if device is not None else (None, None)
+        obj_dict.update(
+            device_type=device_type,
+            device_index=device_index,
+        )
 
         return MSGPackSerializer.dumps(obj_dict)
 
@@ -115,7 +115,7 @@ class BatchTensorDescriptor(TensorDescriptor):
         if obj_dict["layout"] is not None:
             obj_dict["layout"] = _str_to_torch_type(obj_dict["layout"], torch.layout)
 
-        if "device_type" in obj_dict:
+        if obj_dict["device_type"] is not None:
             obj_dict["device"] = torch.device(
                 obj_dict.pop("device_type"),
                 obj_dict.pop("device_index"),
