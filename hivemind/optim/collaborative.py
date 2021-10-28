f023abcc44b3c0b629ae350150f3ed95aa09fda4
justheuristic <justheuristic@gmail.com>
2021-10-28 04:02:26 +0300

    move TrainingAverager to optim

--- hivemind/optim/collaborative.py
+++ hivemind/optim/collaborative.py
@@ -9,11 +9,11 @@ import numpy as np
 import torch
 from pydantic import BaseModel, StrictBool, StrictFloat, confloat, conint
 
-from hivemind.averaging.training import TrainingAverager
 from hivemind.dht import DHT
 from hivemind.dht.crypto import RSASignatureValidator
 from hivemind.dht.schema import BytesWithPublicKey, SchemaValidator
 from hivemind.optim.base import DecentralizedOptimizerBase
+from hivemind.optim.training import TrainingAverager
 from hivemind.optim.grad_scaler import HivemindGradScaler
 from hivemind.optim.performance_ema import PerformanceEMA
 from hivemind.utils import get_dht_time, get_logger
