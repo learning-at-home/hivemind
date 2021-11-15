ef3d2b54c20e709cf396007783d1598be719a7bd
justheuristic <justheuristic@gmail.com>
2021-11-15 15:49:55 +0300

    missing docstrings

--- tests/test_optimizer.py
+++ tests/test_optimizer.py
@@ -125,10 +125,8 @@ def test_state_averager(offload_optimizer, reuse_tensors):
     assert not torch.allclose(stats1, stats2)
 
     avgr2.step(increment_epoch=True)
-
     avgr1.step(increment_epoch=True, averaging_round=True, delay_averaging=True)
     avgr2.step(increment_epoch=True, averaging_round=True, delay_averaging=True)
-
     avgr1.step(wait_for_delayed_update=True)
     avgr2.step(wait_for_delayed_update=True)
 
