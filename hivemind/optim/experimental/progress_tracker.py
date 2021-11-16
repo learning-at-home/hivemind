c511464a8439d6664ff37ec84e313f336761b7dc
justheuristic <justheuristic@gmail.com>
2021-11-16 19:59:30 +0300

    Update hivemind/optim/experimental/progress_tracker.py
    Co-authored-by: Alexander Borzunov <hxrussia@gmail.com>

--- hivemind/optim/experimental/progress_tracker.py
+++ hivemind/optim/experimental/progress_tracker.py
@@ -239,7 +239,7 @@ class ProgressTracker(Thread):
             logger.log(self.status_loglevel, f"No longer fetching {self.training_progress_key}.")
 
     def _parse_swarm_progress_data(
-        self, metadata: Optional[Dict[BytesWithPublicKey, Optional[LocalTrainingProgress]]]
+        self, metadata: TrainingProgressSchema
     ) -> GlobalTrainingProgress:
         """Read performance statistics reported by peers, estimate progress towards next batch"""
         current_time = get_dht_time()
