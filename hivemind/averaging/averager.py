001ef12ba4451ad88cbf36e8c930a8a0bab6ae08
justheuristic <justheuristic@gmail.com>
2021-11-25 05:49:24 +0300

    check if culprit is the declaration coro

--- hivemind/averaging/averager.py
+++ hivemind/averaging/averager.py
@@ -565,12 +565,11 @@ class DecentralizedAverager(mp.Process, ServicerBase):
             yield message
 
     async def _declare_for_download_periodically(self):
+        return#TODO
         download_key = f"{self._matchmaking.group_key_manager.prefix}.all_averagers"
         sharing_was_allowed = self.allow_state_sharing
         while True:
             self._should_redeclare_state_sharing.clear()
-            expiration_time = get_dht_time() + self.declare_state_period
-
             if self.allow_state_sharing or sharing_was_allowed:
                 # notify either if sharing is allowed or if it was just switched off (to overwrite previous message)
                 asyncio.create_task(
@@ -579,7 +578,7 @@ class DecentralizedAverager(mp.Process, ServicerBase):
                             download_key,
                             subkey=self.peer_id.to_bytes(),
                             value=self.state_sharing_priority if self.allow_state_sharing else None,
-                            expiration_time=expiration_time,
+                            expiration_time=get_dht_time() + self.declare_state_period,
                             return_future=True,
                         ),
                         timeout=self.declare_state_period - self.request_timeout,
