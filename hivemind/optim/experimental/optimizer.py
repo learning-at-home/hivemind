095b386777d595377f34e10985e391ac73d1c684
justheuristic <justheuristic@gmail.com>
2021-12-04 05:35:21 +0300

    DEBUG do not preschedule state averagign

--- hivemind/optim/experimental/optimizer.py
+++ hivemind/optim/experimental/optimizer.py
@@ -545,6 +545,7 @@ class Optimizer(torch.optim.Optimizer):
 
     def _maybe_schedule_state_averaging(self) -> None:
         """If next epoch is coming soon, schedule the next state averaging at estimated parameter averaging start"""
+        return
         next_epoch = max(self.local_epoch + 1, self.tracker.global_epoch)
         if next_epoch % self.average_state_every != 0:
             return  # averaging is not performed at this epoch
