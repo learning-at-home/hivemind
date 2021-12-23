9c7b1f14964f45c0d20b7c88c57114fd7c72c576
Mais Alheraki <mais@fairybits.com>
2021-12-24 01:52:43 +0300

    Raise error if user attempts to specify incorrect port in announce_maddrs

--- hivemind/p2p/p2p_daemon.py
+++ hivemind/p2p/p2p_daemon.py
@@ -140,6 +140,11 @@ class P2P:
         socket_uid = secrets.token_urlsafe(8)
         self._daemon_listen_maddr = Multiaddr(cls._UNIX_SOCKET_PREFIX + f"p2pd-{socket_uid}.sock")
         self._client_listen_maddr = Multiaddr(cls._UNIX_SOCKET_PREFIX + f"p2pclient-{socket_uid}.sock")
+        if announce_maddrs is not None:
+            for addr in announce_maddrs:
+                if addr.startswith("/ip4/") or addr.startswith("/ip6/"):
+                    if addr.endswith("/tcp/0") or addr.endswith("/udp/0/quic"):
+                        raise ValueError("Please specify an explicit port in announce_maddrs: port 0 is not supported")
 
         need_bootstrap = bool(initial_peers) or use_ipfs
         process_kwargs = cls.DHT_MODE_MAPPING.get(dht_mode, {"dht": 0})
