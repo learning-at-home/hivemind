4ffd9ca65c3e040c9599f4c64c0b960190c42250
justheuristic <justheuristic@gmail.com>
2022-01-19 09:01:28 +0300

    Update optimizer.py

--- hivemind/optim/optimizer.py
+++ hivemind/optim/optimizer.py
@@ -529,6 +529,7 @@ class Optimizer(torch.optim.Optimizer):
                 self._tag_along_with_zero_weight(self.scheduled_grads)
             else:
                 logger.log(self.status_loglevel, f"Skipping pre-scheduled averaging round: there are no other peers")
+                self.grad_averager.load_accumulators_into_averager_()
                 self.scheduled_grads.cancel()
             self.scheduled_grads = None
         return began_averaging_gradients
