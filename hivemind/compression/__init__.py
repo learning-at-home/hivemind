609fd30f060d3b2401e052bf3642f97e90dd2098
Aleksandr Borzunov <borzunov.alexander@gmail.com>
2022-03-04 14:38:12 +0000

    isort

--- hivemind/compression/__init__.py
+++ hivemind/compression/__init__.py
@@ -6,4 +6,4 @@ from hivemind.compression.adaptive import PerTensorCompression, RoleAdaptiveComp
 from hivemind.compression.base import CompressionBase, CompressionInfo, NoCompression, TensorRole
 from hivemind.compression.floating import Float16Compression, ScaledFloat16Compression
 from hivemind.compression.quantization import Quantile8BitQuantization, Uniform8BitQuantization
-from hivemind.compression.serialization import serialize_torch_tensor, deserialize_torch_tensor
+from hivemind.compression.serialization import deserialize_torch_tensor, serialize_torch_tensor
