7aaf726b7e141de19c13f6627d1b41017e459903
Max Ryabinin <mryabinin0@gmail.com>
2022-06-04 20:32:45 +0300

    Add get_free port to imports from utils.networking

--- hivemind/utils/__init__.py
+++ hivemind/utils/__init__.py
@@ -3,7 +3,7 @@ from hivemind.utils.limits import increase_file_limit
 from hivemind.utils.logging import get_logger, use_hivemind_log_handler
 from hivemind.utils.mpfuture import *
 from hivemind.utils.nested import *
-from hivemind.utils.networking import log_visible_maddrs
+from hivemind.utils.networking import log_visible_maddrs, get_free_port
 from hivemind.utils.performance_ema import PerformanceEMA
 from hivemind.utils.serializer import MSGPackSerializer, SerializerBase
 from hivemind.utils.streaming import combine_from_streaming, split_for_streaming
