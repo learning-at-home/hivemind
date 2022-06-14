fa2da451bf08d4705b5acfa7aea8f8e171348c99
justheuristic <justheuristic@gmail.com>
2022-06-14 07:25:34 +0300

    rename

--- hivemind/moe/server/expert_backend.py
+++ hivemind/moe/server/expert_backend.py
@@ -56,7 +56,7 @@ class ExpertBackend:
         self.args_schema = args_schema = tuple(args_schema or ())
         self.kwargs_schema = kwargs_schema = dict(kwargs_schema or {})
         assert args_schema or kwargs_schema, (
-            f"{self.__class__.__name__} must receive at least one positional or keyword input."
+            f"Module must take at least one positional or keyword input."
             " Did you forget to provide args_schema/kwargs_schema?"
         )
 
