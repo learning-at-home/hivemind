8701aded97a89bf19fc37469a0a18d25fcd8274b
Max Ryabinin <mryabinin0@gmail.com>
2022-06-16 15:31:18 +0300

    Sort imports

--- tests/test_utils/p2p_daemon.py
+++ tests/test_utils/p2p_daemon.py
@@ -11,6 +11,7 @@ from multiaddr import Multiaddr, protocols
 from pkg_resources import resource_filename
 
 from hivemind.p2p.p2p_daemon_bindings.p2pclient import Client
+
 from test_utils.networking import get_free_port
 
 TIMEOUT_DURATION = 30  # seconds
