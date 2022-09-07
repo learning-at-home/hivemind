6ec8d60754ec9432e41619970cb1399e792ccb23
justheuristic <justheuristic@gmail.com>
2022-09-07 17:20:11 +0300

    Update task_pool.py

--- hivemind/moe/server/task_pool.py
+++ hivemind/moe/server/task_pool.py
@@ -170,7 +170,7 @@ class TaskPool(TaskPoolBase):
             for skip_i in range(prev_num_tasks):
                 finished_task_timestamp = (
                     self.undispatched_task_timestamps.get()
-                )  # earlier timestamp = lower (better) priority
+                )  # earlier timestamp = lower (faster) priority
                 if skip_i == prev_num_tasks - 1:
                     self.priority = finished_task_timestamp
 
