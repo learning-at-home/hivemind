1eacdb9765db6f95b609f67feda138f0b470a3db
Alexander Borzunov <borzunov.alexander@gmail.com>
2022-12-02 19:08:09 +0400

    Update p2pd to v0.3.13

--- setup.py
+++ setup.py
@@ -13,14 +13,14 @@ from setuptools import find_packages, setup
 from setuptools.command.build_py import build_py
 from setuptools.command.develop import develop
 
-P2PD_VERSION = "v0.3.12"
+P2PD_VERSION = "v0.3.13"
 
 P2PD_SOURCE_URL = f"https://github.com/learning-at-home/go-libp2p-daemon/archive/refs/tags/{P2PD_VERSION}.tar.gz"
 P2PD_BINARY_URL = f"https://github.com/learning-at-home/go-libp2p-daemon/releases/download/{P2PD_VERSION}/"
 
 # The value is sha256 of the binary from the release page
 EXECUTABLES = {
-    "p2pd": "2530c3b4afe7953a6c73878d718808edc51c1f0de814e6730d8e53fbbc30789c",
+    "p2pd": "c3bf9bffdf649ec295f515ed744882d7e197220846a21825e8f11c007f01dbc0",
 }
 
 here = os.path.abspath(os.path.dirname(__file__))
