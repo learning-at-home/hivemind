b769f89ef267f392b013f8235c38355141b2f264
Your Name <you@example.com>
2023-07-20 23:26:26 +0300

    black it

--- hivemind/averaging/matchmaking.py
+++ hivemind/averaging/matchmaking.py
@@ -480,7 +480,10 @@ class PotentialLeaders:
                 self.peer_id.to_bytes(),
             ):
                 await asyncio.wait(
-                    {asyncio.create_task(self.update_finished.wait()), asyncio.create_task(self.declared_expiration.wait())},
+                    {
+                        asyncio.create_task(self.update_finished.wait()),
+                        asyncio.create_task(self.declared_expiration.wait()),
+                    },
                     return_when=asyncio.FIRST_COMPLETED,
                 )
                 self.declared_expiration.clear()
