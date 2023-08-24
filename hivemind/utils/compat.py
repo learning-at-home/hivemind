93d8f0e6dc04ac7c5fb8353091335b9d665d22fb
Aleksandr Borzunov <hxrussia@gmail.com>
2023-08-24 13:15:50 +0000

    Handle "Ran out of input"

--- hivemind/utils/compat.py
+++ hivemind/utils/compat.py
@@ -2,6 +2,10 @@ import multiprocessing as mp
 import time
 from typing import Any
 
+from hivemind.utils.logging import get_logger
+
+logger = get_logger(__name__)
+
 
 def safe_recv(pipe: mp.connection.Connection) -> Any:
     # Needed for macOS, see https://github.com/urllib3/urllib3/issues/63#issuecomment-4609289
@@ -9,8 +13,12 @@ def safe_recv(pipe: mp.connection.Connection) -> Any:
     while True:
         try:
             return pipe.recv()
-        except BlockingIOError as e:
-            if str(e) == "[Errno 35] Resource temporarily unavailable":
+        except Exception as e:
+            if (
+                (isinstance(e, BlockingIOError) and str(e) == "[Errno 35] Resource temporarily unavailable") or
+                (isinstance(e, EOFError) and str(e) == "Ran out of input")
+            ):
+                logger.warning(repr(e))
                 time.sleep(0)
                 continue
             raise
