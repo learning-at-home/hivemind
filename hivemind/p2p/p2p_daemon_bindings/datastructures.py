0e0ef8681978b5bd616265b4440b5f530d4a3943
Denis Mazur <denismazur8@gmail.com>
2023-12-01 02:27:18 +0300

    black

--- hivemind/p2p/p2p_daemon_bindings/datastructures.py
+++ hivemind/p2p/p2p_daemon_bindings/datastructures.py
@@ -20,22 +20,6 @@ from hivemind.proto import crypto_pb2, p2pd_pb2
 # ENABLE_INLINING = False
 MAX_INLINE_KEY_LENGTH = 42
 
-# IDENTITY_MULTIHASH_CODE = 0x00
-
-# if ENABLE_INLINING:
-
-#     class IdentityHash:
-#         def __init__(self) -> None:
-#             self._digest = bytearray()
-
-#         def update(self, input: bytes) -> None:
-#             self._digest += input
-
-#         def digest(self) -> bytes:
-#             return self._digest
-
-#     multihash.FuncReg.register(IDENTITY_MULTIHASH_CODE, "identity", hash_new=IdentityHash)
-
 
 class PeerID:
     def __init__(self, peer_id_bytes: bytes) -> None:
@@ -108,11 +92,10 @@ class PeerID:
             data=encoded_public_key,
         ).SerializeToString()
 
-        #algo = multihash.Func.sha2_256
-        #if ENABLE_INLINING and len(encoded_public_key) <= MAX_INLINE_KEY_LENGTH:
-        #    algo = IDENTITY_MULTIHASH_CODE
-        #encoded_digest = multihash.digest(encoded_public_key, algo).encode()
-        encoded_digest = multihash.encode(hashlib.sha256(encoded_public_key).digest(), multihash.coerce_code("sha2-256"))
+        encoded_digest = multihash.encode(
+            hashlib.sha256(encoded_public_key).digest(),
+            multihash.coerce_code("sha2-256"),
+        )
         return cls(encoded_digest)
 
 
