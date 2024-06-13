b2d95c8f0187092d3ea597d4d8b8987600a9a9ae
Luciferian Ink <LuciferianInk@protonmail.com>
2024-06-13 18:37:51 -0500

    nit

--- tests/test_utils/p2p_daemon.py
+++ tests/test_utils/p2p_daemon.py
@@ -15,7 +15,6 @@ from hivemind.p2p.p2p_daemon_bindings.p2pclient import Client
 from test_utils.networking import get_free_port
 
 TIMEOUT_DURATION = 30  # seconds
-
 P2PD_PATH = resource_filename("hivemind", "hivemind_cli/p2pd")
 
 
