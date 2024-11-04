57b512c0680b5f82a8b09951f48fb0a4c7e9e42d
Denis Mazur <denismazur8@gmail.com>
2024-11-04 16:14:41 +0300

    fix import

--- hivemind/p2p/p2p_daemon_bindings/datastructures.py
+++ hivemind/p2p/p2p_daemon_bindings/datastructures.py
@@ -13,6 +13,7 @@ from cryptography.hazmat.primitives import serialization
 from multiaddr import Multiaddr
 
 from hivemind.proto import crypto_pb2, p2pd_pb2
+from hivemind.p2p.multiaddr import Multiaddr
 
 
 class PeerID:
