f21827bd58b37b5d3936d18be3f45220647d5bf2
Max Ryabinin <mryabinin0@gmail.com>
2025-04-20 14:24:56 +0200

    Remove the fixture override

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,3 @@
-import asyncio
 import gc
 
 import psutil
@@ -10,22 +9,6 @@ from hivemind.utils.mpfuture import MPFuture
 
 use_hivemind_log_handler("in_root_logger")
 logger = get_logger(__name__)
-
-
-@pytest.fixture
-def event_loop():
-    """
-    This overrides the ``event_loop`` fixture from pytest-asyncio
-    (e.g. to make it compatible with ``asyncio.subprocess``).
-
-    This fixture is identical to the original one but does not call ``loop.close()`` in the end.
-    Indeed, at this point, the loop is already stopped (i.e. next tests are free to create new loops).
-    However, finalizers of objects created in the current test may reference the current loop and fail if it is closed.
-    For example, this happens while using ``asyncio.subprocess`` (the ``asyncio.subprocess.Process`` finalizer
-    fails if the loop is closed, but works if the loop is only stopped).
-    """
-
-    yield asyncio.get_event_loop()
 
 
 @pytest.fixture(autouse=True, scope="session")
