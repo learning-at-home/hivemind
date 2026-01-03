cdded7683c3ef0f1dd74f52d234a8c77881f3f4d
Max Ryabinin <mryabinin0@gmail.com>
2026-01-03 16:10:35 +0100

    Revert control.py, move sharing strategy to hivemind/__init__.py
    - Revert hivemind/averaging/control.py to torch-based implementation
      (averaging module already depends on torch, no benefit from SharedMemory)
    - Move torch.multiprocessing.set_sharing_strategy() call from mpfuture.py
      to hivemind/__init__.py (global setting for all torch shared memory)

--- hivemind/__init__.py
+++ hivemind/__init__.py
@@ -1,3 +1,9 @@
+import os
+
+import torch
+
+torch.multiprocessing.set_sharing_strategy(os.environ.get("HIVEMIND_MEMORY_SHARING_STRATEGY", "file_system"))
+
 from hivemind.averaging import DecentralizedAverager
 from hivemind.dht import DHT
 from hivemind.moe import (
